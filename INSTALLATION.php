3f814d0c0605806875e2551e39a05810d19afd92
soeren_nb <soeren_nb@87cc427b-4a1e-0410-b729-b2260ec0f063>
2005-11-03 21:14:36 +0000

    VirtueMart readme update
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@108 87cc427b-4a1e-0410-b729-b2260ec0f063

---
+++ INSTALLATION.php
@@ -0,0 +1,18 @@
+<?php
+defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' ); 
+/**
+* @version $Id$
+* @package VirtueMart
+* @subpackage core
+* @copyright Copyright (C) 2004-2005 Soeren Eberhardt. All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+* VirtueMart is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+*
+* http://virtuemart.net
+*/
+?>
+The installation procedure is explained in the file
+README.php
--- README.php
+++ README.php
@@ -17,14 +17,11 @@ defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.'
 <pre>
 ****************
 VirtueMart
-Version: 0.9.0
+Version: 1.0.x
 ****************
-
-based on mambo-phpShop 1.2 stable (patch level 3)
-
 Complete Package for Mambo >= 4.5.1 and Joomla 1.0.x
 
-You can't use this software on an earlier Mambo version than 4.5.1 (e.g. Mambo 4.5 1.0.9)
+You can't use this software on an earlier Mambo version than 4.5.1 (e.g. Mambo 4.5 1.0.9) or later than 4.5.2.
 without running into serious problems.
 
 Copyright (C) 2004-2005 Soeren Eberhardt. All rights reserved.
@@ -42,16 +39,16 @@ Community Home: http://virtuemart.net
 Package Contents:
 ##########################
 
- * 1 Component (com_phpshop_0.9.0.tar.gz)
+ * 1 Component (com_virtuemart_1.0.0.tar.gz)
    INSTALLATION REQUIRED!
    
- * 1 Main module (mod_phpshop_0.9.0.tar.gz)
+ * 1 Main module (mod_virtuemart_1.0.0.tar.gz)
    INSTALLATION REQUIRED!
    
  * 8 additional Modules
  
  * 2 Mambots 
-   - 1 SearchBot for Integration into Mambo Main Search (phpshop.searchbot.tar.gz), 
+   - 1 SearchBot for Integration into Mambo Main Search (virtuemart.searchbot.tar.gz), 
    - 1 Content Mambot for displaying product details in Mambo content (mosproductsnap.tar.gz)
 
 ##########################
@@ -63,37 +60,34 @@ and can't be used without Joomla / Mambo. It installs fairly easy using the auto
 It's intended for use in small / mid-sized Online businesses / Online-Shops. 
 So every user who wants to build up a Online Store can use this component for selling something to customers.
 
-This package is for New Installations ONLY. 
-You need a working Joomla/Mambo Installation. 
-You can get your copy  of Joomla from http://joomla.org
+This package is for New Installations and updates from mambo-phpShop. 
 
-There's a separate Package available for
-   * Manual Installations and Updates
+You just need a working Joomla/Mambo Installation. 
+You can get your copy  of Joomla from http://joomla.org
 
 This package contains some code from the original 0.8.0 Edikon Corp. phpShop distribution available at www.phpshop.org
 
 This package was tested on 
 - Mambo 4.5.1a
 - Mambo 4.5.2.3
-- Joomla 1.0.0 / 1.0.1
+- Joomla 1.0.x
 
  -- IMPORTANT --
 Please note that module and component SHOULD be used together! 
-The thing is that you can only access all areas of the component via the mambo-phpShop Main Module links.
+The thing is that you can only access all areas of the component via the VirtueMart Main Module links.
 
-You can surely create a new Menu Item linking to mambo-phpShop, but then you must Publish the mambo-phpShop module.
-
+You can surely create a new Menu Item linking to VirtueMart, but you must also publish the VirtueMart module.
 
 ##########################
    INSTALLATION
 ##########################
-The installation is really easy - thanks to the
-automatic installer!
+The installation is really easy - 
+thanks to the automatic installer!
 You don't need to unpack any of the archives in this complete package!
 
-1. If you have unpacked this archive (virtuemart_x.x_COMPLETE_PACKAGE.zip), 
+1. If you have unpacked this archive (VirtueMart_x.x_COMPLETE_PACKAGE.zip), 
 	you see a lot of other archives.
-	- com_phpshop_x.xxx.tar.gz, 
+	- com_virtuemart_x.xxx.tar.gz, 
 	- some files beginning with mod_*.tar.gz 
 	- 2 other Packages (these are the so-called Mambots).
     
@@ -104,7 +98,7 @@ You don't need to unpack any of the archives in this complete package!
 	
 	You can see an upload form now.
 	Select the file 
-	- com_phpshop_x.xx.tar.gz 
+	- com_virtuemart_x.xx.tar.gz 
 	and click 'Upload Component'
 	
 	If everything is ok, you should see a "Welcome ..." Screen.
@@ -123,7 +117,7 @@ You don't need to unpack any of the archives in this complete package!
 	your all modules. So now head on to 
 	"Modules" => "Site Modules".
 	You should somewhere see a module entry for "Virtuemart Module"
-	with "mod_phpshop" at the end of that row.
+	with "mod_virtuemart" at the end of that row.
 	If necessary, browse to the next page of the module list.
 	If you've found the module, please select it's checkbox and click
 	on "Edit" in the toolbar.
@@ -140,36 +134,36 @@ You don't need to unpack any of the archives in this complete package!
 
         IF successful, the installer will have created the following directory structure:
         
-         /components/com_phpshop
+         /components/com_virtuemart
          - contains code for non-administrative surfing and ordering
          
-         /components/com_phpshop/shop_image
-         /components/com_phpshop/shop_image/vendor
+         /components/com_virtuemart/shop_image
+         /components/com_virtuemart/shop_image/vendor
          - contains the vendor images
          
-         /components/com_phpshop/js
+         /components/com_virtuemart/js
          - contains JavaScript Files
          
-         /components/com_phpshop/shop_image/product
+         /components/com_virtuemart/shop_image/product
          - contains the product images
          
-         /components/com_phpshop/shop_image/ps_image
+         /components/com_virtuemart/shop_image/ps_image
          - contains general administration/shop images
          
-         /components/com_phpshop/shop_image/stars
+         /components/com_virtuemart/shop_image/stars
          
-         /administrator/components/com_phpshop
+         /administrator/components/com_virtuemart
          - contains config files and the main phpshop-parser
          
-         /administrator/components/com_phpshop/classes
+         /administrator/components/com_virtuemart/classes
          - contains all the classes (formerly the /lib/*.inc - directories)
          
-         /administrator/components/com_phpshop/html
+         /administrator/components/com_virtuemart/html
          - contains all the "ihtml" files (formerly the /html - directories)
          
-         /administrator/components/com_phpshop/html/templates
+         /administrator/components/com_virtuemart/html/templates
          
-         /administrator/components/com_phpshop/languages
+         /administrator/components/com_virtuemart/languages
          - contains the language files
           
          /modules/dtree
@@ -192,7 +186,7 @@ You don't need to unpack any of the archives in this complete package!
 ############################
    DEVELOPER INFORMATION
 ###########################
-For all developers and users dealing with mambo-phpShop, it's important to know that VirtueMart 
+For all developers and users who worked with mambo-phpShop, it's important to know that VirtueMart 
 is the successor of mambo-phpShop. So you as a developer must know how to update your extensions, 
 modules and/or hacks to work in VirtueMart.
 
@@ -253,7 +247,7 @@ and copy all necessary information from mos_users to the renamed table mos_vm_us
 #
 
 For updates / changes / hints please read the ChangeLog!
-
+based on mambo-phpShop 1.2 stable (patch level 3)
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
