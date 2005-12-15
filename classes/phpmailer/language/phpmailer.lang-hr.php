9e51d984100efd501e1fcf60a8974c3a9c4a9714
soeren_nb <soeren_nb@87cc427b-4a1e-0410-b729-b2260ec0f063>
2005-12-15 21:01:05 +0000

    croatian update
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@191 87cc427b-4a1e-0410-b729-b2260ec0f063

---
+++ classes/phpmailer/language/phpmailer.lang-hr.php
@@ -0,0 +1,24 @@
+<?php
+defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
+/**
+ * PHPMailer language file.  
+ * English Version
+ */
+
+$PHPMAILER_LANG = array();
+
+$PHPMAILER_LANG["provide_address"] = 'Morate unijeti barem jednu ' .
+                                     'email adresu primatelja.';
+$PHPMAILER_LANG["mailer_not_supported"] = ' mailer nije podržan.';
+$PHPMAILER_LANG["execute"] = 'Nije moguæe izvršiti: ';
+$PHPMAILER_LANG["instantiate"] = 'Nije moguæe inicirati mail funkciju.';
+$PHPMAILER_LANG["authenticate"] = 'SMTP Greška: Nije moguæe autorizirati.';
+$PHPMAILER_LANG["from_failed"] = 'Adrese pošiljatelja nisu pronaðene: ';
+$PHPMAILER_LANG["recipients_failed"] = 'SMTP Greška: Slijedeæi ' .
+                                       'primatelji nisu pronaðeni: ';
+$PHPMAILER_LANG["data_not_accepted"] = 'SMTP Greška: Data not accepted.';
+$PHPMAILER_LANG["connect_host"] = 'SMTP Greška: Nije se moguæe spojiti na SMTP host.';
+$PHPMAILER_LANG["file_access"] = 'Nije moguæe pristupiti datoteci: ';
+$PHPMAILER_LANG["file_open"] = 'Greška: Nije moguæe otvoriti datoteku: ';
+$PHPMAILER_LANG["encoding"] = 'Nepoznati encoding: ';
+?>
