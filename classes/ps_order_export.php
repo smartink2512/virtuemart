70d6e3880b8c3414862b6d32395ed6c9b53a4b70
mdennerlein <mdennerlein@87cc427b-4a1e-0410-b729-b2260ec0f063>
2006-02-03 23:13:21 +0000

    added functions to handle export classes
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@235 87cc427b-4a1e-0410-b729-b2260ec0f063

--- classes/ps_order_export.php
+++ classes/ps_order_export.php
@@ -27,7 +27,7 @@ class ps_order_export {
 	* @author Manfred Dennerlein Rodelo <manni@zapto.de>
 	*/
 	function validate_add(&$d) {
-
+		global $vmLogger, $VM_LANG;
 		$db = new ps_DB;
 
 		if (!$d['order_export_name']) {
@@ -37,7 +37,10 @@ class ps_order_export {
 		if (empty($d['export_enabled'])) {
 			$d['export_enabled'] = 'N';
 		}
-
+		if(!file_exists( CLASSPATH.'export/'.$d['order_export_class'].'.php' ) ) {
+			$d['error'] = 'ERROR: Export Class does not exist.';
+			return false;
+		}
 		$d['order_export_config'] = mosGetParam( $_POST, 'order_export_config', '', _MOS_ALLOWHTML );
 		if( !get_magic_quotes_runtime() || !get_magic_quotes_gpc() ) {
 			$d['order_export_config'] = addslashes( $d['order_export_config'] );
@@ -51,7 +54,7 @@ class ps_order_export {
 	* @author Manfred Dennerlein Rodelo <manni@zapto.de>
 	*/
 	function validate_delete($d) {
-
+		global $vmLogger, $VM_LANG;
 		if (!$d['order_export_id']) {
 			$d['error'] = 'ERROR:  Please select an export module to delete.';
 			return False;
@@ -68,6 +71,7 @@ class ps_order_export {
 	* @author Manfred Dennerlein Rodelo <manni@zapto.de>
 	*/
 	function validate_update(&$d) {
+		global $vmLogger, $VM_LANG;
 		$db = new ps_DB;
 
 		if (!$d['order_export_id']) {
@@ -78,7 +82,10 @@ class ps_order_export {
 			$d['error'] = 'ERROR:  You must enter a name for the order export module.';
 			return False;
 		}
-
+		if(!file_exists( CLASSPATH.'export/'.$d['order_export_class'].'.php' ) ) {
+			$d['error'] = 'ERROR: Export Class does not exist.';
+			return false;
+		}
 		$d['order_export_config'] = mosGetParam( $_POST, 'order_export_config', '', _MOS_ALLOWHTML );
 		if( !get_magic_quotes_runtime() || !get_magic_quotes_gpc() ) {
 			$d['order_export_config'] = addslashes( $d['order_export_config'] );
@@ -94,6 +101,7 @@ class ps_order_export {
 	* @author Manfred Dennerlein Rodelo <manni@zapto.de>
 	*/
 	function add(&$d) {
+		global $vmLogger, $VM_LANG;
 		$db = new ps_DB;
 		$ps_vendor_id = $_SESSION['ps_vendor_id'];
 		$timestamp = time();
@@ -101,6 +109,23 @@ class ps_order_export {
 		if (!$this->validate_add($d)) {
 			return False;
 		}
+		if ( !empty($d['order_export_class']) ) {
+			// Here we have a custom export class
+			if( file_exists( CLASSPATH.'export/'.$d['order_export_class'].'.php' ) ) {
+				// Include the class code and create an instance of this class
+				include( CLASSPATH.'export/'.$d['order_export_class'].'.php' );
+				eval( "\$_EXPORT = new ".$d['order_export_class']."();");
+			}
+		}
+		else {
+			// ps_xmlexport is the default export method handler
+			include( CLASSPATH."export/ps_xmlexport.php" );
+			$_EXPORT = new ps_payment();
+		}
+		if( $_EXPORT->configfile_writeable() ) {
+			$_EXPORT->write_configuration( $d );
+		}
+
 		$q = 'INSERT INTO #__{vm}_order_export (vendor_id, order_export_name,';
 		$q .= 'order_export_desc, order_export_class, export_enabled, order_export_config, iscore) ';
 		$q .= 'VALUES (';
@@ -124,6 +149,7 @@ class ps_order_export {
 	* @author Manfred Dennerlein
 	*/
 	function update(&$d) {
+		global $vmLogger, $VM_LANG;
 		$db = new ps_DB;
 		$ps_vendor_id = $_SESSION['ps_vendor_id'];
 		$timestamp = time();
@@ -131,6 +157,24 @@ class ps_order_export {
 		if (!$this->validate_update($d)) {
 			return False;
 		}
+
+		if ( !empty($d['order_export_class']) ) {
+			if (include( CLASSPATH.'export/'.$d['order_export_class'].'.php' ))
+			eval( "\$_EXPORT = new ".$d['order_export_class']."();");
+		}
+		else {
+			include( CLASSPATH.'export/ps_xmlexport.php' );
+			$_EXPORT = new ps_xmlexport();
+		}
+		if( $_EXPORT->configfile_writeable() ) {
+			$_EXPORT->write_configuration( $d );
+			$vmLogger->info( $VM_LANG->_VM_CONFIGURATION_CHANGE_SUCCESS );
+		}
+		else {
+			$vmLogger->err( sprintf($VM_LANG->_VM_CONFIGURATION_CHANGE_FAILURE , CLASSPATH."export/".$_EXPORT->classname.".cfg.php" ) );
+			return false;
+		}
+
 		$q = 'UPDATE #__{vm}_order_export SET ';
 		if(!$d['iscore']) {
 			$q .= "order_export_name='" . $d['order_export_name'];
@@ -171,7 +215,7 @@ class ps_order_export {
 			return $this->delete_record( $record_id, $d );
 		}
 	}
-	
+
 	/**
 	* delete order export module update
 	* @param array
