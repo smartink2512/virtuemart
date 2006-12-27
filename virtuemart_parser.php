6b622c8be38ca70ecbc3edf26e03878a8a6c6ff9
soeren_nb <soeren_nb@87cc427b-4a1e-0410-b729-b2260ec0f063>
2006-12-27 11:16:26 +0000

    fixed an SQL injection problem, because category_id and product_id are not properly converted to int
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@594 87cc427b-4a1e-0410-b729-b2260ec0f063

--- virtuemart_parser.php
+++ virtuemart_parser.php
@@ -71,8 +71,8 @@ if( !defined( '_VM_PARSER_LOADED' )) {
 	
 		unset( $_REQUEST["error"] );
 		$user_id = intval( mosgetparam($_REQUEST, 'user_id', 0) );
-		$_SESSION['session_userstate']['product_id'] = $product_id = intval( mosgetparam($_REQUEST, 'product_id', 0) );
-		$_SESSION['session_userstate']['category_id'] = $category_id = intval( mosgetparam($_REQUEST, 'category_id', 0) );
+		$_SESSION['session_userstate']['product_id'] = $product_id = $_REQUEST['product_id'] = intval( mosgetparam($_REQUEST, 'product_id', 0) );
+		$_SESSION['session_userstate']['category_id'] = $category_id = $_REQUEST['category_id'] = intval( mosgetparam($_REQUEST, 'category_id', 0) );
 		$user_info_id = mosgetparam($_REQUEST, 'user_info_id', 0);
 
 		$myInsecureArray = array('keyword' => $keyword,
