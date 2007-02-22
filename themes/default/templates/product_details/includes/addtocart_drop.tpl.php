22b90f84f14fd191a9abb4f49bcca2c94d193175
macallf <macallf@87cc427b-4a1e-0410-b729-b2260ec0f063>
2007-02-22 13:09:43 +0000

    template for drop down children
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@697 87cc427b-4a1e-0410-b729-b2260ec0f063

---
+++ themes/default/templates/product_details/includes/addtocart_drop.tpl.php
@@ -0,0 +1,22 @@
+<?php defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' ); ?>
+
+<div class="vmCartDetails<?php echo $cls_suffix ?>">
+<?php if(USE_AS_CATALOGUE != '1' && ($advanced_attribute != "" || $custom_attribute !="")) { ?>
+<div class="vmCartChild<?php echo $cls_suffix ?> vmRowTwo<?php echo $cls_suffix ?>">
+<? }
+echo $drop_down ?>
+<?php if(USE_AS_CATALOGUE != '1' && ($advanced_attribute != "" || $custom_attribute != "")) { ?>
+<div class=\"vmCartAttributes<?php echo $cls_suffix ?>">
+<?php   if($advanced_attribute) {
+            echo $advanced_attribute;
+        }
+        if($custom_attribute) {
+            echo $custom_attribute;
+        }
+?>
+</div>
+<?php } ?>
+<?php if(USE_AS_CATALOGUE != '1' && ($advanced_attribute != "" || $custom_attribute !="")) { ?>
+</div>
+<? }?> 
+</div>
