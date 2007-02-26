d981b1438b4e516deed11a8362d35fc1a8d04da3
gregdev <gregdev@87cc427b-4a1e-0410-b729-b2260ec0f063>
2007-02-26 14:40:25 +0000

    Added templates for account.billing, account.orders, account.shipping.
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@724 87cc427b-4a1e-0410-b729-b2260ec0f063

---
+++ themes/default/templates/pages/account.billing.tpl.php
@@ -0,0 +1,48 @@
+<?php
+defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' ); 
+/**
+*
+* @version $Id: account.billing.tpl.php 663 2007-02-06 06:52:29 -0700 (Tue, 06 Feb 2007) soeren_nb $
+* @package VirtueMart
+* @subpackage html
+* @copyright Copyright (C) 2004-2007 Soeren Eberhardt. All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+* VirtueMart is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See /administrator/components/com_virtuemart/COPYRIGHT.php for copyright notices and details.
+*
+* http://virtuemart.net
+*/
+
+?>
+<div style="float:left;width:90%;text-align:right;"> 
+    <span>
+    	<a href="#" onclick="if( submitregistration() ) { document.adminForm.submit();}">
+    		<img border="0" src="administrator/images/save_f2.png" name="submit" alt="<?php echo $VM_LANG->_E_SAVE ?>" />
+    	</a>
+    </span>
+    <span style="margin-left:10px;">
+    	<a href="<?php $sess->purl( SECUREURL."index.php?page=$next_page") ?>">
+    		<img src="administrator/images/back_f2.png" alt="<?php echo $VM_LANG->_BACK ?>" border="0" />
+    	</a>
+    </span>
+</div>
+
+<?php
+ps_userfield::listUserFields( $fields, array(), $db );
+?>
+
+<div align="center">	
+	<input type="submit" value="<?php echo $VM_LANG->_CMN_SAVE ?>" class="button" onclick="return( submitregistration());" />
+</div>
+  <input type="hidden" name="option" value="<?php echo $option ?>" />
+  <input type="hidden" name="page" value="<?php echo $next_page; ?>" />
+  <input type="hidden" name="Itemid" value="<?php echo $Itemid; ?>" />
+  <input type="hidden" name="func" value="shopperupdate" />
+  <input type="hidden" name="user_info_id" value="<?php $db->p("user_info_id"); ?>" />
+  <input type="hidden" name="id" value="<?php echo $auth["user_id"] ?>" />
+  <input type="hidden" name="user_id" value="<?php echo $auth["user_id"] ?>" />
+  <input type="hidden" name="address_type" value="BT" />
+</form>
