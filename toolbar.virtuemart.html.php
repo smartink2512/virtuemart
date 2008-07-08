1e738c0667c81677e54052048a6aeb9075585d9d
soeren_nb <soeren_nb@87cc427b-4a1e-0410-b729-b2260ec0f063>
2008-07-08 19:31:54 +0000

    fixed Syntax Error
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@1454 87cc427b-4a1e-0410-b729-b2260ec0f063

--- toolbar.virtuemart.html.php
+++ toolbar.virtuemart.html.php
@@ -5,7 +5,7 @@ if( !defined( '_VALID_MOS' ) && !defined( '_JEXEC' ) ) die( 'Direct Access to '.
 * @version $Id$
 * @package VirtueMart
 * @subpackage core
-* @copyright Copyright (C) 2004-2007 soeren - All rights reserved.
+* @copyright Copyright (C) 2004-2008 soeren - All rights reserved.
 * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
 * VirtueMart is free software. This version may have been modified pursuant
 * to the GNU General Public License, and as distributed it includes or
@@ -44,7 +44,7 @@ class MENU_virtuemart {
 	*/
     function FORMS_MENU_SAVE_CANCEL() {     
         global $mosConfig_absolute_path,$mosConfig_live_site, $mosConfig_lang, $VM_LANG, 
-        		$product_id, page, $limitstart,	$mosConfig_editor, $vmIcons;
+        		$product_id, $page, $limitstart,	$mosConfig_editor, $vmIcons;
 
 		$bar = & JToolBar::getInstance('toolbar');
 		
