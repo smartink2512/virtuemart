7b3a75e577b3bf2fd13a482b4be897a0dbbe6789
bass28 <bass28@87cc427b-4a1e-0410-b729-b2260ec0f063>
2009-12-23 20:17:40 +0000

    Newly converted views enabled on landing page
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@2116 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart/views/virtuemart/tmpl/default.php
+++ administrator/components/com_virtuemart/views/virtuemart/tmpl/default.php
@@ -13,12 +13,12 @@ echo $pane->startPanel(JText::_('VM_CONTROL_PANEL'), 'control_panel');
 <div id="cpanel">
 	<div class="icon"><?php ImageHelper::displayImageButton(JROUTE::_('index.php?option=com_virtuemart&view=product'), 'vm_shop_products_48.png', JText::_('VM_PRODUCT_LIST_LBL')); ?></div>
 	<div class="icon"><?php ImageHelper::displayImageButton(JROUTE::_('index.php?option=com_virtuemart&pshop_mode=admin&page=product.product_category_list'), 'vm_shop_categories_48.png', JText::_('VM_CATEGORY_LIST_LBL')); ?></div>
-	<div class="icon"><?php ImageHelper::displayImageButton(JROUTE::_('index.php?option=com_virtuemart&pshop_mode=admin&page=order.order_list'), 'vm_shop_orders_48.png', JText::_('VM_ORDER_MOD')); ?></div>
+	<div class="icon"><?php ImageHelper::displayImageButton(JROUTE::_('index.php?option=com_virtuemart&view=orders'), 'vm_shop_orders_48.png', JText::_('VM_ORDER_MOD')); ?></div>
 	<div class="icon"><?php ImageHelper::displayImageButton(JROUTE::_('index.php?option=com_virtuemart&pshop_mode=admin&page=store.payment_method_list'), 'vm_shop_payment_48.png', JText::_('VM_PAYMENT_METHOD_LIST_MNU')); ?></div>
 	<div class="icon"><?php ImageHelper::displayImageButton(JROUTE::_('index.php?option=com_virtuemart&view=product'), 'vm_shop_vendors_48.png', JText::_('VM_VENDOR_MOD')); ?></div>
 	<div class="icon"><?php ImageHelper::displayImageButton(JROUTE::_('index.php?option=com_virtuemart&pshop_mode=admin&page=admin.user_list'), 'vm_shop_users_48.png', JText::_('VM_USERS')); ?></div>
 	<div class="icon"><?php ImageHelper::displayImageButton(JROUTE::_('index.php?option=com_virtuemart&pshop_mode=admin&page=admin.show_cfg'), 'vm_shop_configuration_48.png', JText::_('VM_CONFIG')); ?></div>
-	<div class="icon"><?php ImageHelper::displayImageButton(JROUTE::_('index.php?option=com_virtuemart&pshop_mode=admin&page=store.store_form'), 'vm_shop_mart_48.png', JText::_('VM_STORE_FORM_MNU')); ?></div>
+	<div class="icon"><?php ImageHelper::displayImageButton(JROUTE::_('index.php?option=com_virtuemart&view=store'), 'vm_shop_mart_48.png', JText::_('VM_STORE_FORM_MNU')); ?></div>
 	<div class="icon"><?php ImageHelper::displayImageButton(JROUTE::_('http://virtuemart.org/index.php?option=com_content&amp;task=view&amp;id=248&amp;Itemid=125'), 'vm_shop_help_48.png', JText::_('VM_HELP_MOD')); ?></div>
 </div>
 <?php
@@ -60,7 +60,7 @@ echo $pane->startPanel(JText::_('VM_STATISTIC_STATISTICS'), 'statistics_page');
 		</tr>
 		<tr>
 			<th colspan="2" class="title">
-		  		<a href="<?php echo JROUTE::_('index.php?option=com_virtuemart&page=order.order_list');?>">
+		  		<a href="<?php echo JROUTE::_('index.php?option=com_virtuemart&view=orders');?>">
 					<?php echo JText::_('VM_ORDER_MOD') ?>
 				</a>
 			</th>
