e321d0888997ddaca8ee32f102f54d2e5c07c1aa
bass28 <bass28@87cc427b-4a1e-0410-b729-b2260ec0f063>
2009-12-27 03:36:04 +0000

    Fix for install error
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@2136 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart/models/updatesMigration.php
+++ administrator/components/com_virtuemart/models/updatesMigration.php
@@ -13,7 +13,7 @@ defined('_JEXEC') or die('Restricted access');
 
 jimport( 'joomla.application.component.model');
 require_once(JPATH_ROOT.DS.'administrator'.DS.'components'.DS.'com_virtuemart'.DS.'helpers'.DS.'vendorHelper.php');
-require_once(JPATH_COMPONENT_ADMINISTRATOR.DS.'helpers'.DS.'connection.php');
+require_once(JPATH_ROOT.DS.'administrator'.DS.'components'.DS.'com_virtuemart'.DS.'helpers'.DS.'connection.php');
 
 /**
  * Model class for updates and migrations
