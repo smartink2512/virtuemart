6fb163680951fce0112f869e2946a6c4742cbd94
jseros <jseros@87cc427b-4a1e-0410-b729-b2260ec0f063>
2009-12-27 02:56:57 +0000

    Improving markup on uninstall message
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@2135 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart/install/uninstall.virtuemart.html.php
+++ administrator/components/com_virtuemart/install/uninstall.virtuemart.html.php
@@ -1,13 +1,12 @@
-<?php/**
+<?php
+/**
  * VirtueMart uninstall HTML file.
  *
  * @author Max Milbers, RickG
  * @package VirtueMart
- */ ?>
- 
-<link rel="stylesheet" href="components/com_virtuemart/assets/css/install.css" type="text/css" /> 
-
-<div align="center">
+ */ 
+?>
+<div>
 	<table width="100%" border="0">
 	<tr>
 		<td>
@@ -16,11 +15,12 @@
 	</tr>
 	<tr>
 		<td valign="top" align="center">
-			<br />
-			<b>Uninstall Note:</b><br />
-			Uninstalling Virtuemart does not remove the Virtuemart tables from your database.&nbsp;&nbsp;
-			This is done intentionally  to allow for upgrading your current data to new versions of Virtuemart.&nbsp;&nbsp;
-			To remove the Virtuemart tables remove all tables with the _vm_ prefix.
+			<h2>Uninstall Note:</h2>
+			<p>
+				<big>Uninstalling Virtuemart does not remove the Virtuemart tables from your database.&nbsp;&nbsp;
+				This is done intentionally  to allow for upgrading your current data to new versions of Virtuemart.&nbsp;&nbsp;
+				To remove the Virtuemart tables remove all tables with the _vm_ prefix.</big>
+			</p>
 		</td>
 	</tr>	
 	</table>
