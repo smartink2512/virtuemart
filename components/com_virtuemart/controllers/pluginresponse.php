e62aacd52e83362ae434f8b6693406b46bb463a5
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2012-06-19 18:19:17 +0000

    oops .. not that one yet
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@6136 87cc427b-4a1e-0410-b729-b2260ec0f063

--- components/com_virtuemart/controllers/pluginresponse.php
+++ components/com_virtuemart/controllers/pluginresponse.php
@@ -87,26 +87,6 @@ class VirtueMartControllerPluginresponse extends JController {
 
     function ShipmentResponseReceived() {
 
-	    if (!class_exists('vmPSPlugin'))
-		    require(JPATH_VM_PLUGINS . DS . 'vmpsplugin.php'); JPluginHelper::importPlugin('vmshipment');
-
-	    $return_context = "";
-	    $dispatcher = JDispatcher::getInstance();
-	    $html = "";
-	    $shipmentResponse = Jtext::_('COM_VIRTUEMART_CART_THANKYOU');
-	    $dispatcher->trigger('plgVmOnShipmentResponseReceived', array( 'html' => &$html,&$shipmentResponse));
-
-// 	JRequest::setVar('paymentResponse', Jtext::_('COM_VIRTUEMART_CART_THANKYOU'));
-// 	JRequest::setVar('paymentResponseHtml', $html);
-	    $view = $this->getView('pluginresponse', 'html');
-	    $layoutName = JRequest::getVar('layout', 'default');
-	    $view->setLayout($layoutName);
-
-	    $view->assignRef('shipmentResponse', $shipmentResponse);
-	    $view->assignRef('shipmentResponseHtml', $html);
-
-	    // Display it all
-	    $view->display();
     }
 
     /**
