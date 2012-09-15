ca83c6b8d61f3505948ca1bc37b00ed7a6d419be
Milbo <Milbo@87cc427b-4a1e-0410-b729-b2260ec0f063>
2012-09-15 08:57:39 +0000

    unitprice had a bug if you stored 100 ml or 100 g
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@6455 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart/helpers/shopfunctions.php
+++ administrator/components/com_virtuemart/helpers/shopfunctions.php
@@ -468,12 +468,12 @@ class ShopFunctions {
 
 		$weight_unit_default = array(
 			'KG' => JText::_ ('COM_VIRTUEMART_UNIT_SYMBOL_KG')
-		, '100MG' => JText::_ ('COM_VIRTUEMART_UNIT_SYMBOL_100MG')
+		, 'DMG' => JText::_ ('COM_VIRTUEMART_UNIT_SYMBOL_100MG')
 		, 'M'   => JText::_ ('COM_VIRTUEMART_UNIT_SYMBOL_M')
 		, 'SM'   => JText::_ ('COM_VIRTUEMART_UNIT_SYMBOL_SM')
 		, 'CUBM'   => JText::_ ('COM_VIRTUEMART_UNIT_SYMBOL_CUBM')
 		, 'L'   => JText::_ ('COM_VIRTUEMART_UNIT_SYMBOL_L')
-		, '100ML'   => JText::_ ('COM_VIRTUEMART_UNIT_SYMBOL_100ML')
+		, 'DML'   => JText::_ ('COM_VIRTUEMART_UNIT_SYMBOL_100ML')
 		);
 		foreach ($weight_unit_default as  $key => $value) {
 			$wu_list[] = JHTML::_ ('select.option', $key, $value, $name);
