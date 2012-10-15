da1dbe0fe3d9c4d52bc013ac3dcdb775b8ffe964
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2012-10-15 07:37:49 +0000

    plugins: changing ü to ue. Paypal: trying to understand why paypal (and only paypal) is removed when installing the AIO
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@6537 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart_allinone/script.vmallinone.php
+++ administrator/components/com_virtuemart_allinone/script.vmallinone.php
@@ -51,10 +51,10 @@ if (!defined ('_VM_SCRIPT_INCLUDED')) {
 			$this->path = JInstaller::getInstance ()->getPath ('extension_administrator');
 
 			$this->updateShipperToShipment ();
-			$this->installPlugin ('Standard', 'plugin', 'standard', 'vmpayment');
+			$this->installPlugin ('Standard', 'plugin',  'standard', 'vmpayment');
+			$this->installPlugin ('Paypal',    'plugin', 'paypal',    'vmpayment');
 			$this->installPlugin ('PayZen', 'plugin', 'payzen', 'vmpayment');
 			$this->installPlugin ('SystemPay', 'plugin', 'systempay', 'vmpayment');
-
 			//moneybookers
 			$src = $this->path . DS . 'plugins' . DS . 'vmpayment' . DS . 'moneybookers';
 			$dst = JPATH_ROOT . DS . 'plugins' . DS . 'vmpayment' . DS . 'moneybookers';
@@ -63,15 +63,16 @@ if (!defined ('_VM_SCRIPT_INCLUDED')) {
 			$this->installPlugin ('Moneybookers Lastschrift', 'plugin', 'moneybookers_did', 'vmpayment');
 			$this->installPlugin ('Moneybookers iDeal', 'plugin', 'moneybookers_idl', 'vmpayment');
 			$this->installPlugin ('Moneybookers Giropay', 'plugin', 'moneybookers_gir', 'vmpayment');
-			$this->installPlugin ('Moneybookers Sofortüberweisung', 'plugin', 'moneybookers_sft', 'vmpayment');
+			$this->installPlugin ('Moneybookers Sofortueberweisung', 'plugin', 'moneybookers_sft', 'vmpayment');
 			$this->installPlugin ('Moneybookers Przelewy24', 'plugin', 'moneybookers_pwy', 'vmpayment');
 			$this->installPlugin ('Moneybookers Online Bank Transfer', 'plugin', 'moneybookers_obt', 'vmpayment');
 			$this->installPlugin ('Moneybookers Skrill Digital Wallet', 'plugin', 'moneybookers_wlt', 'vmpayment');
 			$this->installPlugin ('Authorize.net', 'plugin', 'authorizenet', 'vmpayment');
-			$this->installPlugin ('Paypal', 'plugin', 'paypal', 'vmpayment');
 			$this->installPlugin ('Klarna', 'plugin', 'klarna', 'vmpayment');
 			$this->installPlugin ('Heidelpay', 'plugin', 'heidelpay', 'vmpayment');
+
 			$this->installPlugin ('By weight, ZIP and countries', 'plugin', 'weight_countries', 'vmshipment');
+
 			$this->installPlugin ('Customer text input', 'plugin', 'textinput', 'vmcustom');
 			$this->installPlugin ('Product specification', 'plugin', 'specification', 'vmcustom');
 			$this->installPlugin ('Stockable variants', 'plugin', 'stockable', 'vmcustom');
