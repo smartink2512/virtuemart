cfcb9717387a29281f8fabd69e3ebbfdae6ea464
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2013-01-04 13:18:16 +0000

    2014 MP:  Paypal: Ipn notification via SSL + 443
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/branches/com_virtuemart2.0.14mp@6797 87cc427b-4a1e-0410-b729-b2260ec0f063

--- plugins/vmpayment/paypal/paypal.php
+++ plugins/vmpayment/paypal/paypal.php
@@ -179,18 +179,18 @@ class plgVmPaymentPaypal extends vmPSPlugin {
 			$quantity = $quantity + $product->quantity;
 		}
 		$post_variables = Array(
-			'cmd'               => '_ext-enter',
-			'redirect_cmd'      => '_xclick',
-			'upload'            => '1', //Indicates the use of third-party shopping cart
-			'business'          => $merchant_email, //Email address or account ID of the payment recipient (i.e., the merchant).
-			'receiver_email'    => $merchant_email, //Primary email address of the payment recipient (i.e., the merchant
-			'order_number'      => $order['details']['BT']->order_number,
-			"invoice"           => $order['details']['BT']->order_number,
-			'custom'            => $return_context,
-			'item_name'         => JText::_ ('VMPAYMENT_PAYPAL_ORDER_NUMBER') . ': ' . $order['details']['BT']->order_number,
+			'cmd'              => '_ext-enter',
+			'redirect_cmd'     => '_xclick',
+			'upload'           => '1', //Indicates the use of third-party shopping cart
+			'business'         => $merchant_email, //Email address or account ID of the payment recipient (i.e., the merchant).
+			'receiver_email'   => $merchant_email, //Primary email address of the payment recipient (i.e., the merchant
+			'order_number'     => $order['details']['BT']->order_number,
+			"invoice"          => $order['details']['BT']->order_number,
+			'custom'           => $return_context,
+			'item_name'        => JText::_ ('VMPAYMENT_PAYPAL_ORDER_NUMBER') . ': ' . $order['details']['BT']->order_number,
 			//'quantity'          => $quantity,
-			"amount"            => $totalInPaymentCurrency,
-			"currency_code"     => $currency_code_3,
+			"amount"           => $totalInPaymentCurrency,
+			"currency_code"    => $currency_code_3,
 			/*
 					 * 1 – L'adresse spécifiée dans les variables pré-remplies remplace l'adresse de livraison enregistrée auprès de PayPal.
 					 * Le payeur voit l'adresse qui est transmise mais ne peut pas la modifier.
@@ -198,29 +198,29 @@ class plgVmPaymentPaypal extends vmPSPlugin {
 					 * (par exemple si des champs requis, tel que le pays, sont manquants) ou pas incluse.
 					 * Valeurs autorisées : 0, 1. Valeur par défaut : 0
 					 */
-			"address_override"  => isset($method->address_override) ? $method->address_override : 0, // 0 ??   Paypal does not allow your country of residence to ship to the country you wish to
-			"first_name"        => $address->first_name,
-			"last_name"         => $address->last_name,
-			"address1"          => $address->address_1,
-			"address2"          => isset($address->address_2) ? $address->address_2 : '',
-			"zip"               => $address->zip,
-			"city"              => $address->city,
-			"state"             => isset($address->virtuemart_state_id) ? ShopFunctions::getStateByID ($address->virtuemart_state_id) : '',
-			"country"           => ShopFunctions::getCountryByID ($address->virtuemart_country_id, 'country_2_code'),
-			"email"             => $order['details']['BT']->email,
-			"night_phone_b"     => $address->phone_1,
-			"return"            => JROUTE::_ (JURI::root () . 'index.php?option=com_virtuemart&view=pluginresponse&task=pluginresponsereceived&on=' . $order['details']['BT']->order_number . '&pm=' . $order['details']['BT']->virtuemart_paymentmethod_id . '&Itemid=' . JRequest::getInt ('Itemid')),
+			"address_override" => isset($method->address_override) ? $method->address_override : 0, // 0 ??   Paypal does not allow your country of residence to ship to the country you wish to
+			"first_name"       => $address->first_name,
+			"last_name"        => $address->last_name,
+			"address1"         => $address->address_1,
+			"address2"         => isset($address->address_2) ? $address->address_2 : '',
+			"zip"              => $address->zip,
+			"city"             => $address->city,
+			"state"            => isset($address->virtuemart_state_id) ? ShopFunctions::getStateByID ($address->virtuemart_state_id) : '',
+			"country"          => ShopFunctions::getCountryByID ($address->virtuemart_country_id, 'country_2_code'),
+			"email"            => $order['details']['BT']->email,
+			"night_phone_b"    => $address->phone_1,
+			"return"           => JROUTE::_ (JURI::root () . 'index.php?option=com_virtuemart&view=pluginresponse&task=pluginresponsereceived&on=' . $order['details']['BT']->order_number . '&pm=' . $order['details']['BT']->virtuemart_paymentmethod_id . '&Itemid=' . JRequest::getInt ('Itemid')),
 			// Keep this line, needed when testing
 			//"return" => JROUTE::_(JURI::root() . 'index.php?option=com_virtuemart&view=pluginresponse&task=pluginnotification&tmpl=component'),
-			"notify_url"        => JROUTE::_ (JURI::root () . 'index.php?option=com_virtuemart&view=pluginresponse&task=pluginnotification&tmpl=component'),
-			"cancel_return"     => JROUTE::_ (JURI::root () . 'index.php?option=com_virtuemart&view=pluginresponse&task=pluginUserPaymentCancel&on=' . $order['details']['BT']->order_number . '&pm=' . $order['details']['BT']->virtuemart_paymentmethod_id . '&Itemid=' . JRequest::getInt ('Itemid')),
+			"notify_url"       => JROUTE::_ (JURI::root () . 'index.php?option=com_virtuemart&view=pluginresponse&task=pluginnotification&tmpl=component'),
+			"cancel_return"    => JROUTE::_ (JURI::root () . 'index.php?option=com_virtuemart&view=pluginresponse&task=pluginUserPaymentCancel&on=' . $order['details']['BT']->order_number . '&pm=' . $order['details']['BT']->virtuemart_paymentmethod_id . '&Itemid=' . JRequest::getInt ('Itemid')),
 			//"undefined_quantity" => "0",
-			"ipn_test"          => $method->debug,
-			"rm"                => '2', // the buyer’s browser is redirected to the return URL by using the POST method, and all payment variables are included
+			"ipn_test"         => $method->debug,
+			"rm"               => '2', // the buyer’s browser is redirected to the return URL by using the POST method, and all payment variables are included
 			//"pal" => "NRUBJXESJTY24",
-			"image_url"         => JURI::root () . $vendor->images[0]->file_url,
-			"no_shipping"       => isset($method->no_shipping) ? $method->no_shipping : 0,
-			"no_note"           => "1");
+			"image_url"        => JURI::root () . $vendor->images[0]->file_url,
+			"no_shipping"      => isset($method->no_shipping) ? $method->no_shipping : 0,
+			"no_note"          => "1");
 
 		/*
 					  $i = 1;
@@ -439,44 +439,36 @@ class plgVmPaymentPaypal extends vmPSPlugin {
 		}
 		$paypal_data = JRequest::get ('post');
 		if (!isset($paypal_data['invoice'])) {
-			return NULL;
+			return FALSE;
 		}
+
 		$order_number = $paypal_data['invoice'];
 		if (!($virtuemart_order_id = VirtueMartModelOrders::getOrderIdByOrderNumber ($paypal_data['invoice']))) {
-			return NULL;
+			return FALSE;
 		}
 
 		$vendorId = 0;
 		if (!($payments = $this->getDatasByOrderId ($virtuemart_order_id))) {
-			return NULL;
+			return FALSE;
 		}
 
 		$method = $this->getVmPluginMethod ($payments[0]->virtuemart_paymentmethod_id);
 		if (!$this->selectedThisElement ($method->payment_element)) {
 			return FALSE;
 		}
-		if (isset($method->log_ipn) and $method->log_ipn) {
-			$this->logIpn ();
-		}
 		$this->_debug = $method->debug;
 
 		$this->logInfo ('paypal_data ' . implode ('   ', $paypal_data), 'message');
+		// _processIPN checks that  $res== "VERIFIED"
+		if (!$this->_processIPN ($paypal_data, $method)) {
+			$this->logInfo ('paypal_data _processIPN FALSE', 'message');
+			return FALSE;
+		}
 
 		//$this->_storePaypalInternalData ($method, $paypal_data, $virtuemart_order_id, $payment->virtuemart_paymentmethod_id);
 		$modelOrder = VmModel::getModel ('orders');
 		$order = array();
 
-		// _processIPN checks that  $res== "VERIFIED"
-		$error_msg = $this->_processIPN ($paypal_data, $method);
-
-		$this->logInfo ('process IPN ' . $error_msg, 'message');
-
-		if (!(empty($error_msg))) {
-			$this->logInfo ('process IPN ' . $error_msg . ' ' . $order['order_status'], 'ERROR');
-			return NULL;
-		} else {
-			$this->logInfo ('process IPN OK', 'message');
-		}
 		/*
 		 * https://cms.paypal.com/us/cgi-bin/?cmd=_render-content&content_ID=developer/e_howto_html_IPNandPDTVariables
 		 * The status of the payment:
@@ -493,9 +485,7 @@ class plgVmPaymentPaypal extends vmPSPlugin {
 		 * Voided: This authorization has been voided.
 		 *
 		 */
-		if (empty($paypal_data['payment_status']) || ($paypal_data['payment_status'] != 'Completed' && $paypal_data['payment_status'] != 'Pending')) {
-			//return false;
-		}
+
 		$lang = JFactory::getLanguage ();
 		$order['customer_notified'] = 1;
 
@@ -503,11 +493,11 @@ class plgVmPaymentPaypal extends vmPSPlugin {
 		if (strcmp ($paypal_data['payment_status'], 'Completed') == 0) {
 			// 2. check that txn_id has not been previously processed
 			if ($this->_check_txn_id_already_processed ($payments, $paypal_data['txn_id'], $method)) {
-				return;
+				return FALSE;
 			}
 			// 3. check email and amount currency is correct
 			if (!$this->_check_email_amount_currency ($payments, $this->_getMerchantEmail ($method), $paypal_data)) {
-				return;
+				return FALSE;
 			}
 			// now we can process the payment
 			$order['order_status'] = $method->status_success;
@@ -663,11 +653,22 @@ class plgVmPaymentPaypal extends vmPSPlugin {
 	 * @return string Empty string if data is valid and an error message otherwise
 	 * @access protected
 	 */
-	function _processIPN ($paypal_data, $method) {
+	private function _processIPN ($paypal_data, $method) {
 
-		$secure_post = $method->secure_post;
+		// check that the remote IP is from Paypal.
+		if (!$this->checkPaypalIps ($paypal_data['ipn_test'], $paypal_data['invoice'], $method)) {
+			$this->logInfo ('_processIPN checkPaypalIps FALSE', 'message');
+			return FALSE;
+		}
+		// Paypal wants to open the socket in SSL
+		$port = 443;
+		$protocol = 'ssl://';
 		$paypal_url = $this->_getPaypalURL ($method);
-		// read the post from PayPal system and add 'cmd'
+		/*
+		 * Before we can trust the contents of the message, we must first verify that the message came from PayPal.
+		 * To verify the message, we must send back the contents in the exact order they
+		*  were received and precede it with the command _notify-validate,
+		 */
 		$post_msg = 'cmd=_notify-validate';
 		foreach ($paypal_data as $key => $value) {
 			if ($key != 'view' && $key != 'layout') {
@@ -675,51 +676,63 @@ class plgVmPaymentPaypal extends vmPSPlugin {
 				$post_msg .= "&$key=$value";
 			}
 		}
-
-		$this->checkPaypalIps ($paypal_data['ipn_test']);
-
-		// post back to PayPal system to validate
-		$header = "POST /cgi-bin/webscr HTTP/1.0\r\n";
+/*
+				$header = "POST /cgi-bin/webscr HTTP/1.0\r\n";
+				$header .= "Content-Type: application/x-www-form-urlencoded\r\n";
+				$header .= "Content-Length: " . strlen ($post_msg) . "\r\n\r\n";
+*/
+		 $header = "POST /cgi-bin/webscr HTTP/1.0\r\n";
+		$header .= "User-Agent: PHP/" . phpversion () . "\r\n";
+		$header .= "Referer: " . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'] . @$_SERVER['QUERY_STRING'] . "\r\n";
+		$header .= "Server: " . $_SERVER['SERVER_SOFTWARE'] . "\r\n";
+		$header .= "Host: "  . $this->_getPaypalUrl ($method) . ":" . $port . "\r\n";
 		$header .= "Content-Type: application/x-www-form-urlencoded\r\n";
-		$header .= "Content-Length: " . strlen ($post_msg) . "\r\n\r\n";
+		$header .= "Content-Length: " . strlen ($post_msg) . "\r\n";
+		$header .= "Accept: */*\r\n\r\n";
 
-		if ($secure_post) {
-			// If possible, securely post back to paypal using HTTPS
-			// Your PHP server will need to be SSL enabled
-			$fps = fsockopen ('ssl://' . $paypal_url, 443, $errno, $errstr, 30);
-		} else {
-			$fps = fsockopen ($paypal_url, 80, $errno, $errstr, 30);
-		}
+		$fps = fsockopen ($protocol . $paypal_url, $port, $errno, $errstr, 30);
 
+		$valid_ipn = false;
 		if (!$fps) {
 			$this->sendEmailToVendorAndAdmins ("error with paypal", JText::sprintf ('VMPAYMENT_PAYPAL_ERROR_POSTING_IPN', $errstr, $errno));
-			return JText::sprintf ('VMPAYMENT_PAYPAL_ERROR_POSTING_IPN', $errstr, $errno); // send email
+			$this->logInfo ('_processIPN fsockopen FALSE', 'message');
 		} else {
 			fputs ($fps, $header . $post_msg);
+			$this->logInfo ('_processIPN Fputs: ' . $header . $post_msg, 'message');
+			$res = '';
 			while (!feof ($fps)) {
-				$res = fgets ($fps, 1024);
+				$res .= fgets ($fps, 1024);
+			}
+			fclose ($fps);
 
-				if (strcmp ($res, 'VERIFIED') == 0) {
-					return '';
-				} elseif (strcmp ($res, 'INVALID') == 0) {
+			$this->logInfo ('_processIPN FROM IPN VALIDATION:' . $res, 'message');
+			// Inspect IPN validation result and act accordingly
+			$valid_ipn = strstr ($res, "VERIFIED");
+			if (!$valid_ipn) {
+				if (strstr ($res, "INVALID")) {
 					$emailBody = "Hello,\n\nerror with paypal IPN NOTIFICATION" . " " . $res . "\n";
 					// If 'INVALID', send an email. TODO: Log for manual investigation.
 					foreach ($paypal_data as $key => $value) {
 						$emailBody .= $key . " = " . $value . "\n";
 					}
 					$this->sendEmailToVendorAndAdmins (JText::_ ('VMPAYMENT_PAYPAL_ERROR_IPN_VALIDATION') . " " . $res, $emailBody);
-					return JText::_ ('VMPAYMENT_PAYPAL_ERROR_IPN_VALIDATION') . $res;
+					$this->logInfo ('_processIPN INVALID', 'message');
+				} else {
+					$emailBody = "Hello,
+                An error occured while processing a paypal transaction.";
+					$this->sendEmailToVendorAndAdmins (JText::_ ('VMPAYMENT_PAYPAL_ERROR_IPN_VALIDATION') . " " . $res, $emailBody);
+					$this->logInfo ('_processIPN NO ANSWER FROM PAYPAL', 'message');
 				}
 			}
 		}
 
-		fclose ($fps);
-		return '';
+		$this->logInfo ('_processIPN valid_ipn:' . $valid_ipn, 'message');
+		return $valid_ipn;
 	}
 
 	function _check_txn_id_already_processed ($payments, $txn_id, $method) {
 
-		$virtuemart_order_id = $payments[0] ->virtuemart_order_id;
+		$virtuemart_order_id = $payments[0]->virtuemart_order_id;
 		$orderModel = VmModel::getModel ('orders');
 		$order = $orderModel->getOrder ($virtuemart_order_id);
 
@@ -746,13 +759,11 @@ class plgVmPaymentPaypal extends vmPSPlugin {
 			return true;
 		}
 		*/
-		if ($payments[0]->payment_order_total != $paypal_data['mc_gross']) {
-			return FALSE;
-		}
 		$currency_code_3 = shopFunctions::getCurrencyByID ($payments[0]->payment_currency, 'currency_code_3');
-		if ($currency_code_3 != $paypal_data['mc_currency']) {
-			return FALSE;
+		if (($payments[0]->payment_order_total == $paypal_data['mc_gross']) and ($currency_code_3 == $paypal_data['mc_currency'])) {
+			return TRUE;
 		}
+
 		$mailsubject = "PayPal Transaction";
 		$mailbody = "Hello,
 		An IPN notification was received with an invalid amount or currency
@@ -809,61 +820,47 @@ class plgVmPaymentPaypal extends vmPSPlugin {
 	 * @param $test_ipn
 	 * @return mixed
 	 */
-	function checkPaypalIps ($test_ipn) {
+	function checkPaypalIps ($test_ipn, $order_number, $method) {
 
-		return;
 		// Get the list of IP addresses for www.paypal.com and notify.paypal.com
-
-		$paypal_iplist = gethostbynamel ('www.paypal.com');
-		$paypal_iplist2 = gethostbynamel ('notify.paypal.com');
-		$paypal_iplist3 = array('216.113.188.202', '216.113.188.203', '216.113.188.204', '66.211.170.66');
-		$paypal_iplist = array_merge ($paypal_iplist, $paypal_iplist2, $paypal_iplist3);
-
-		$paypal_sandbox_hostname = 'ipn.sandbox.paypal.com';
-		$remote_hostname = gethostbyaddr ($_SERVER['REMOTE_ADDR']);
-
-		$valid_ip = FALSE;
-
-		if ($paypal_sandbox_hostname == $remote_hostname) {
-			$valid_ip = TRUE;
-			$hostname = 'www.sandbox.paypal.com';
+		if ($method->sandbox) {
+			$paypal_iplist = gethostbynamel ('ipn.sandbox.paypal.com');
+			$paypal_iplist = (array)$paypal_iplist;
 		} else {
-			$ips = "";
-			// Loop through all allowed IPs and test if the remote IP connected here
-			// is a valid IP address
-			if (in_array ($_SERVER['REMOTE_ADDR'], $paypal_iplist)) {
-				$valid_ip = TRUE;
-			}
-			$hostname = 'www.paypal.com';
+			$paypal_iplist1 = gethostbynamel ('www.paypal.com');
+			$paypal_iplist2 = gethostbynamel ('notify.paypal.com');
+			$paypal_iplist = array_merge ($paypal_iplist1, $paypal_iplist2);
 		}
-
-		if (!$valid_ip) {
-
-			$mailsubject = "PayPal IPN Transaction on your site: Possible fraud";
-			$mailbody = "Error code 506. Possible fraud. Error with REMOTE IP ADDRESS = " . $_SERVER['REMOTE_ADDR'] . ".
+		$this->logInfo ('checkPaypalIps: ' . implode (",", $paypal_iplist) . " server is:" . $_SERVER['REMOTE_ADDR'], 'message');
+		$hostname = $this->_getPaypalUrl ($method);
+		//  test if the remote IP connected here is a valid IP address
+		if (!in_array ($_SERVER['REMOTE_ADDR'], $paypal_iplist)) {
+			$mail_subject = "PayPal IPN Transaction on your site: Possible fraud";
+			$mail_body = "Error code 506. Possible fraud. Error with REMOTE IP ADDRESS = " . $_SERVER['REMOTE_ADDR'] . ".
                         The remote address of the script posting to this notify script does not match a valid PayPal ip address\n
-            These are the valid IP Addresses: $ips
-
-            The Order ID received was: $invoice";
-
-			$this->sendEmailToVendorAndAdmins ($mailsubject, $mailbody);
-
-			exit();
+            These are the valid IP Addresses: " . implode (",", $paypal_iplist) .
+				"The Order ID received was: " . $order_number;
+			$this->sendEmailToVendorAndAdmins ($mail_subject, $mail_body);
+			return FALSE;
 		}
+		/*
+				if (!($method->sandbox && $test_ipn == 1)) {
+					$res = "FAILED";
+					$mailsubject = "PayPal Sandbox Transaction";
+					$mailbody = "Hello,
+				A fatal error occurred while processing a paypal transaction.
+				----------------------------------
+				Hostname: $hostname
+				URI:" . $_SERVER["REMOTE_ADDR"] .
+						" A Paypal transaction was made using the sandbox without your site in Paypal-Debug-Mode";
+					//vmMail($mosConfig_mailfrom, $mosConfig_fromname, $debug_email_address, $mailsubject, $mailbody );
+					$this->sendEmailToVendorAndAdmins ($mailsubject, $mailbody);
+					return FALSE;
+				}
+		*/
+		$this->logInfo ('checkPaypalIps:  OK', 'message');
 
-		if (!($hostname == "www.sandbox.paypal.com" && $test_ipn == 1)) {
-			$res = "FAILED";
-			$mailsubject = "PayPal Sandbox Transaction";
-			$mailbody = "Hello,
-		A fatal error occurred while processing a paypal transaction.
-		----------------------------------
-		Hostname: $hostname
-		URI: $uri
-		A Paypal transaction was made using the sandbox without your site in Paypal-Debug-Mode";
-			//vmMail($mosConfig_mailfrom, $mosConfig_fromname, $debug_email_address, $mailsubject, $mailbody );
-			$this->sendEmailToVendorAndAdmins ($mailsubject, $mailbody);
-			exit();
-		}
+		return TRUE;
 	}
 
 	/**
