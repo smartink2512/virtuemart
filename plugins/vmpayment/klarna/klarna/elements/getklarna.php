372837588a94c4a16a7da126e685b151239bfeec
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2013-03-07 08:39:28 +0000

    2014 MP: GetKlarna link fixed
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/branches/com_virtuemart2.0.14mp@6855 87cc427b-4a1e-0410-b729-b2260ec0f063

--- plugins/vmpayment/klarna/klarna/elements/getklarna.php
+++ plugins/vmpayment/klarna/klarna/elements/getklarna.php
@@ -58,9 +58,10 @@ class JElementGetKlarna extends JElement {
 */
 		$logo = '<img src="' . JURI::root () . VMKLARNAPLUGINWEBROOT . '/klarna/assets/images/logo/get_klarna_now.jpg" />';
 		$html = '<a href="#" id="klarna_getklarna_link" ">' . $logo . '</a>';
-
+// https://merchants.klarna.com/signup?locale=en&partner_id=7829355537eae268a17667c199e7c7662d3391f7&utm_campaign=Platform&utm_medium=Partners&utm_source=Virtuemart
 		$html .= '<div id="klarna_getklarna_show_hide" >';
-		$url = "https://merchants.klarna.com/signup/choose_region?locale=' . $signLang . '&partner_id=7829355537eae268a17667c199e7c7662d3391f7";
+		$url = "https://merchants.klarna.com/signup/?locale=" . $signLang . "&partner_id=7829355537eae268a17667c199e7c7662d3391f7&utm_campaign=Platform&utm_medium=Partners&utm_source=Virtuemart";
+
 		$js = '
 		jQuery(document).ready(function( $ ) {
 			$("#klarna_getklarna_show_hide").hide();
