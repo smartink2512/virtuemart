3fa748074ed18a1f8a5a8c88dc1bdd712bf0426d
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2013-09-16 15:15:09 +0000

    Sofort naming
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/branches/com_virtuemart2.0.14mp@7203 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart_allinone/script.vmallinone.php
+++ administrator/components/com_virtuemart_allinone/script.vmallinone.php
@@ -60,7 +60,7 @@ if (!defined ('_VM_SCRIPT_INCLUDED')) {
 			$this->updateShipperToShipment ();
 			$this->installPlugin ('Standard', 'plugin', 'standard', 'vmpayment');
 			$this->installPlugin ('Klarna', 'plugin', 'klarna', 'vmpayment');
-			$this->installPlugin ('Sofort Banking', 'plugin', 'sofort', 'vmpayment');
+			$this->installPlugin ('Sofort Banking/Überweisung', 'plugin', 'sofort', 'vmpayment');
 			$this->installPlugin ('Paypal', 'plugin', 'paypal', 'vmpayment');
 			$this->installPlugin ('Heidelpay', 'plugin', 'heidelpay', 'vmpayment');
 			$this->installPlugin ('PayZen', 'plugin', 'payzen', 'vmpayment');
