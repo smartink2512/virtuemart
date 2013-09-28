2acf3d104fb2df6d32e257ae9d392fede966f3ac
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2013-09-28 15:03:34 +0000

    jquery.ui.all.css for the FE (used in shopper fields)
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/branches/com_virtuemart2.0.14mp@7249 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart/helpers/config.php
+++ administrator/components/com_virtuemart/helpers/config.php
@@ -1442,6 +1442,7 @@ class vmJsApi{
 		vmJsApi::js ('jquery.ui.core',FALSE,'',TRUE);
 		vmJsApi::js ('jquery.ui.datepicker',FALSE,'',TRUE);
 
+		vmJsApi::css ('jquery.ui.all',$front.'css/ui' ) ;
 		$lg = JFactory::getLanguage();
 		$lang = $lg->getTag();
 
