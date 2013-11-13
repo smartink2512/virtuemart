ff1748295651e40373ad88b9260b435b59b2ce36
Milbo <Milbo@87cc427b-4a1e-0410-b729-b2260ec0f063>
2013-11-13 17:30:29 +0000

    fix for download of invoices/ delivery
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/branches/com_virtuemart2.0.14mp@7375 87cc427b-4a1e-0410-b729-b2260ec0f063

--- components/com_virtuemart/controllers/invoice.php
+++ components/com_virtuemart/controllers/invoice.php
@@ -64,16 +64,67 @@ class VirtueMartControllerInvoice extends JController
 
 			// Create the invoice PDF file on disk and send that back
 			$orderDetails = $this->getOrderDetails();
-			$fileName = $this->getInvoicePDF($orderDetails);
-			if (file_exists ($fileName)) {
+			$fileLocation = $this->getInvoicePDF($orderDetails, 'invoice',$layout);
+			$fileName = basename ($fileLocation);
+
+			if (file_exists ($fileLocation)) {
+				$maxSpeed = 200;
+				$range = 0;
+				$size = filesize ($fileLocation);
+				$contentType = 'application/pdf';
 				header ("Cache-Control: public");
 				header ("Content-Transfer-Encoding: binary\n");
 				header ('Content-Type: application/pdf');
+
 				$contentDisposition = 'attachment';
-				header ("Content-Disposition: $contentDisposition; filename=\"".basename($fileName)."\"");
-				$contents = file_get_contents ($fileName);
-				echo $contents;
+
+				$agent = strtolower ($_SERVER['HTTP_USER_AGENT']);
+
+				if (strpos ($agent, 'msie') !== FALSE) {
+					$fileName = preg_replace ('/\./', '%2e', $fileName, substr_count ($fileName, '.') - 1);
+				}
+
+				header ("Content-Disposition: $contentDisposition; filename=\"$fileName\"");
+
+				header ("Accept-Ranges: bytes");
+
+				if (isset($_SERVER['HTTP_RANGE'])) {
+					list($a, $range) = explode ("=", $_SERVER['HTTP_RANGE']);
+					str_replace ($range, "-", $range);
+					$size2 = $size - 1;
+					$new_length = $size - $range;
+					header ("HTTP/1.1 206 Partial Content");
+					header ("Content-Length: $new_length");
+					header ("Content-Range: bytes $range$size2/$size");
+				}
+				else {
+					$size2 = $size - 1;
+					header ("Content-Range: bytes 0-$size2/$size");
+					header ("Content-Length: " . $size);
+				}
+
+				if ($size == 0) {
+					die('Zero byte file! Aborting download');
+				}
+
+				//$contents = file_get_contents ($fileName);
+				//echo $contents;
+
+				//	set_magic_quotes_runtime(0);
+				$fp = fopen ("$fileLocation", "rb");
+				fseek ($fp, $range);
+
+				while (!feof ($fp) and (connection_status () == 0)) {
+					set_time_limit (0);
+					print(fread ($fp, 1024 * $maxSpeed));
+					flush ();
+					ob_flush ();
+					sleep (1);
+				}
+				fclose ($fp);
+
 				JFactory::getApplication()->close();
+
 			} else {
 				// TODO: Error message
 				// vmError("File $fileName not found!");
@@ -189,19 +240,6 @@ class VirtueMartControllerInvoice extends JController
 		$view->addTemplatePath( JPATH_VM_SITE.DS.'views'.DS.$viewName.DS.'tmpl' );
 		$vmtemplate = VmConfig::get('vmtemplate',0);
 		$templateName = shopFunctionsF::setTemplate($vmtemplate);
-		/*if($vmtemplate===0 or $vmtemplate == 'default'){
-			if(JVM_VERSION == 2){
-				$q = 'SELECT `template` FROM `#__template_styles` WHERE `client_id`="0" AND `home`="1"';
-			} else {
-				$q = 'SELECT `template` FROM `#__templates_menu` WHERE `client_id`="0" AND `menuid`="0"';
-			}
-			$db = JFactory::getDbo();
-			$db->setQuery($q);
-			$templateName = $db->loadResult();
-		} else {
-			$templateName = $vmtemplate;
-		}*/
-
 
 		if(!empty($templateName)){
 			$TemplateOverrideFolder = JPATH_SITE.DS."templates".DS.$templateName.DS."html".DS."com_virtuemart".DS."invoice";
@@ -209,7 +247,6 @@ class VirtueMartControllerInvoice extends JController
 				$view->addTemplatePath( $TemplateOverrideFolder);
 			}
 		}
-
 
 		$view->invoiceNumber = $invoiceNumberDate[0];
 		$view->invoiceDate = $invoiceNumberDate[1];
@@ -227,8 +264,6 @@ class VirtueMartControllerInvoice extends JController
 				$vendor->vendor_store_name, $view->invoiceNumber, 
 				$orderDetails['details']['BT']->order_number),
 			'keywords' => JText::_('COM_VIRTUEMART_INVOICE_CREATOR'));
-
-		$view->display();
 
 		return VmPdf::createVmPdf($view, $path, 'F', $metadata);
 	}
