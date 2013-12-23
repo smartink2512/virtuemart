24b8a61e23544946a775954506ed910119f50eab
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2013-12-23 13:43:57 +0000

    vmpsplugin: missing including the config file
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/branches/com_virtuemart2.0.14mp@7523 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart/plugins/vmpsplugin.php
+++ administrator/components/com_virtuemart/plugins/vmpsplugin.php
@@ -1330,6 +1330,8 @@ abstract class vmPSPlugin extends vmPlugin {
 	 * Keep it for compatibilty
 	 */
 	protected function logInfo ($text, $type = 'message', $doLog=false) {
+		if (!class_exists( 'VmConfig' )) require(JPATH_COMPONENT_ADMINISTRATOR.DS.'helpers'.DS.'config.php');
+		VmConfig::loadConfig();
 		if ((isset($this->_debug) and $this->_debug) OR $doLog) {
 			$oldLogFileName= 	VmConfig::$logFileName;
 			VmConfig::$logFileName =$this->getLogFileName() ;
