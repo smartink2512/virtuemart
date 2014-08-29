d11183d40c055516477f4764fad2a9dfa54d945e
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2014-08-29 08:03:39 +0000

    Paypal fatal error fix on refund
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/branches/com_virtuemart2.0.14mp@8251 87cc427b-4a1e-0410-b729-b2260ec0f063

--- plugins/vmpayment/paypal/paypal/helpers/paypal.php
+++ plugins/vmpayment/paypal/paypal/helpers/paypal.php
@@ -814,6 +814,10 @@ class PaypalHelperPaypal {
 		}
 	}
 
+	function RefundTransaction ($payment) {
+		return false;
+	}
+
 	function handleResponse () {
 		if ($this->response) {
 			if ($this->response['ACK'] == 'Failure' || $this->response['ACK'] == 'FailureWithWarning') {
