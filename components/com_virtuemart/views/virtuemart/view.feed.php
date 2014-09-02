12955aed0b2f9ab2ca97ab5747291a3d6436cd68
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2014-09-02 20:23:42 +0000

    Feed files and product link
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@8268 87cc427b-4a1e-0410-b729-b2260ec0f063

--- components/com_virtuemart/views/virtuemart/view.feed.php
+++ components/com_virtuemart/views/virtuemart/view.feed.php
@@ -24,7 +24,7 @@ if (!class_exists ('VmView')) {
 	require(JPATH_VM_SITE . DS . 'helpers' . DS . 'vmview.php');
 }
 
-class VirtuemartViewCategory extends VmView {
+class VirtueMartViewVirtueMart extends VmView {
 
 	public function display ($tpl = NULL) {
 
@@ -40,22 +40,37 @@ class VirtuemartViewCategory extends VmView {
 		if (!class_exists('VmImage'))
 			require(JPATH_VM_ADMINISTRATOR . DS . 'helpers' . DS . 'image.php');
 		$productModel = VmModel::getModel ('product');
-		$categoryId = vRequest::getInt ('virtuemart_category_id', false);
-		$feed_show_prices = VmConfig::get ('feed_cat_show_prices', 0);
-		$feed_show_images = VmConfig::get ('feed_cat_show_images', 0);
-		$feed_show_description = VmConfig::get ('feed_cat_show_description', 0);
-		$feed_description_type = VmConfig::get ('feed_cat_description_type', 'product_s_desc');
-		$feed_max_text_length = VmConfig::get ('feed_cat_max_text_length', 0);
-		// Load the products in the given category
-		$products = $productModel->getProductsInCategory ($categoryId);
+		$feed_show_prices = VmConfig::get ('feed_home_show_prices', 0);
+		$feed_show_images = VmConfig::get ('feed_home_show_images', 0);
+		$feed_show_description = VmConfig::get ('feed_home_show_description', 0);
+		$feed_description_type = VmConfig::get ('feed_home_description_type', 'product_s_desc');
+		$feed_max_text_length = VmConfig::get ('feed_home_max_text_length', 0);
+		$products = array();
+		$featured = array();
+		$latest = array();
+		$topten = array();
+
+		if (VmConfig::get ('feed_featured_published', 1)) {
+			$featured_nb = VmConfig::get('feed_featured_nb');
+			$featured = $productModel->getProductListing ('featured', $featured_nb);
+		}
+
+		if (VmConfig::get ('feed_latest_published', 1)) {
+			$latest_nb = VmConfig::get('feed_latest_nb');
+			$latest = $productModel->getProductListing ('latest', $latest_nb);
+		}
+
+		if ( VmConfig::get ('feed_topten_published', 1)) {
+			$topTen_nb = VmConfig::get('feed_topten_nb');
+			$topten = $productModel->getProductListing ('topten',$topTen_nb);
+		}
+		$products = array_merge ($products, $featured, $latest, $topten);
 		if ($feed_show_images == 1) {
 			$productModel->addImages ($products, 1);
 		}
-
 		if ($products && $feed_show_prices == 1) {
 			$currency = CurrencyDisplay::getInstance ();
 		}
-
 		foreach ($products as $product) {
 			$title = $this->escape ($product->product_name);
 			$title = html_entity_decode ($title, ENT_COMPAT, 'UTF-8');
@@ -71,7 +86,7 @@ class VirtuemartViewCategory extends VmView {
 				if ($feed_description_type == 'product_s_desc') {
 					$description .= $product->product_s_desc;
 				} else {
-					if ( $feed_max_text_length > 0) {
+					if ($feed_max_text_length > 0) {
 						$description .= shopFunctionsF::limitStringByWord ($product->product_desc, $feed_max_text_length);
 					} else {
 						$description .= $product->product_desc;
@@ -90,19 +105,19 @@ class VirtuemartViewCategory extends VmView {
 				$description .= $currency->createPriceDiv ('priceWithoutTax', 'COM_VIRTUEMART_PRODUCT_SALESPRICE_WITHOUT_TAX', $product->prices);
 				$description .= $currency->createPriceDiv ('discountAmount', 'COM_VIRTUEMART_PRODUCT_DISCOUNT_AMOUNT', $product->prices);
 				$description .= $currency->createPriceDiv ('taxAmount', 'COM_VIRTUEMART_PRODUCT_TAX_AMOUNT', $product->prices);
-				$unitPriceDescription = vmText::sprintf ('COM_VIRTUEMART_PRODUCT_UNITPRICE', $product->product_unit);
+				$unitPriceDescription = JText::sprintf ('COM_VIRTUEMART_PRODUCT_UNITPRICE', $product->product_unit);
 				$description .= $currency->createPriceDiv ('unitPrice', $unitPriceDescription, $product->prices);
 
 			}
 			if ($feed_description_type == 'product_s_desc'  OR $feed_max_text_length > 0) {
-				$description .= '<p class="feed-readmore"><a target="_blank" href ="' . rtrim (JURI::base (), "/") . str_replace (' ', '%20', $product->link) . '">' . vmText::_ ('COM_VIRTUEMART_FEED_READMORE') . '</a></p>';
+				$description .= '<p class="feed-readmore"><a target="_blank" href ="' . rtrim (JURI::base (), "/") . str_replace (' ', '%20', $product->link) . '">' . JText::_ ('COM_VIRTUEMART_FEED_READMORE') . '</a></p>';
 			}
 			$item = new JFeedItem();
 			$item->title = $title;
 			$item->link = JRoute::_($product->link);
 			$item->date = $product->created_on;
 			$item->description = '<div class="feed-description">' . $description . '</div>';
-			$item->category = $categoryId;
+			$item->category = $product->virtuemart_catgory_id;
 			$doc->addItem ($item);
 		}
 
