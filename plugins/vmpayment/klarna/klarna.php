ce5dcb2533f72be87ca1c0828e203182dd2bcb93
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2014-09-26 13:33:07 +0000

    klarna: Missing include file
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/branches/com_virtuemart2.0.14mp@8337 87cc427b-4a1e-0410-b729-b2260ec0f063

--- plugins/vmpayment/klarna/klarna.php
+++ plugins/vmpayment/klarna/klarna.php
@@ -8,13 +8,17 @@ defined ('_JEXEC') or die();
  * @author Valérie Isaksen
  * @package VirtueMart
  * @link http://www.virtuemart.net
- * @copyright Copyright (C) 2012 iStraxx - All rights reserved.
+ * @copyright Copyright (c) 2004 - 2010 VirtueMart Team. All rights reserved.
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
  * VirtueMart is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
  */
+
+if (!class_exists('VmConfig')) {
+	require(JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_virtuemart' . DS . 'helpers' . DS . 'config.php');
+}
 if (!class_exists ('vmPSPlugin')) {
 	require(JPATH_VM_PLUGINS . DS . 'vmpsplugin.php');
 }
