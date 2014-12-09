97f06c3b9666e4274d59912fde820f03bf6c1514
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2014-12-09 12:04:55 +0000

    standard payment: missing quotes in  tmpl class
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@8616 87cc427b-4a1e-0410-b729-b2260ec0f063

--- plugins/vmpayment/standard/standard/tmpl/post_payment.php
+++ plugins/vmpayment/standard/standard/tmpl/post_payment.php
@@ -19,12 +19,12 @@ defined ('_JEXEC') or die();
 
 ?>
 <div class="post_payment_payment_name" style="width: 100%">
-	<span class=post_payment_payment_name_title"><?php echo vmText::_ ('VMPAYMENT_STANDARD_PAYMENT_INFO'); ?> </span>
+	<span class="post_payment_payment_name_title"><?php echo vmText::_ ('VMPAYMENT_STANDARD_PAYMENT_INFO'); ?> </span>
 	<?php echo  $viewData["payment_name"]; ?>
 </div>
 
 <div class="post_payment_order_number" style="width: 100%">
-	<span class=post_payment_order_number_title"><?php echo vmText::_ ('COM_VIRTUEMART_ORDER_NUMBER'); ?> </span>
+	<span class="post_payment_order_number_title"><?php echo vmText::_ ('COM_VIRTUEMART_ORDER_NUMBER'); ?> </span>
 	<?php echo  $viewData["order_number"]; ?>
 </div>
 
