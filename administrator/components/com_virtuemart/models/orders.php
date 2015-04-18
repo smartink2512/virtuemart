smartink2512/virtuemart

commit 0a6dc1e39748ca2a39006e139eddbce69b9611ce

small cart fix, ensurhing the right shipment/payment is directly set check for safepath is cached better handling of invoice prefix removed doubled code in deleteInvoice, which just calls renameInvoice now
git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@8821 87cc427b-4a1e-0410-b729-b2260ec0f063

administrator/components/com_virtuemart/models/orders.php
@@ -1946,32 +1946,31 @@ $q = 'SELECT virtuemart_order_item_id, product_quantity, order_item_name,
 	/** Rename Invoice  (when an order is deleted)
 	 *
 	 * @author Valérie Isaksen
+	 * @author Max Milbers
 	 * @param $order_id Id of the order
 	 * @return boolean true if deleted successful, false if there was a problem
 	 */
 	function renameInvoice($order_id ) {
-		$db = JFactory::getDBO();
 
-		$q = 'SELECT * FROM `#__virtuemart_invoices` WHERE `virtuemart_order_id`= "'.$order_id.'" ';
-
-		$db->setQuery($q);
-		$data = $db->loadAssoc();
-		if(!$data or   empty($data['invoice_number']) ){
-			return true;
+		$table = $this->getTable('invoices');
+		$table->load($order_id,'virtuemart_order_id');
+		if(empty($table->invoice_number)){
+			return false;
 		}
 
 		// rename invoice pdf file
-		$invoice_prefix='vminvoice_'.VmConfig::$vmlang.'_';
 		$path = shopFunctions::getInvoicePath(VmConfig::get('forSale_path',0));
-		$invoice_name_src = $path.DS.$invoice_prefix.$data['invoice_number'].'.pdf';
+		$name = shopFunctions::getInvoiceName($table->invoice_number);
+		$invoice_name_src = $path.DS.$name.'.pdf';
 
 		if(!file_exists($invoice_name_src)){
 			// may be it was already deleted when changing order items
 			$data['invoice_number'] = "";
+			//$data['invoice_number'] = $data['invoice_number'].' not found.';
 		} else {
 			$date = date("Ymd");
-			$data['invoice_number'] = $data['invoice_number'].'_'.$date;
-			$invoice_name_dst = $path.DS.$data['invoice_number'].'.pdf';
+			$data['invoice_number'] = $table->invoice_number.'_'.$date;
+			$invoice_name_dst = $path.DS.$table->invoice_number.'.pdf';
 
 			if(!class_exists('JFile')) require(VMPATH_LIBS.DS.'joomla'.DS.'filesystem'.DS.'file.php');
 			if (!JFile::move($invoice_name_src, $invoice_name_dst)) {
@@ -1993,39 +1992,8 @@ $q = 'SELECT virtuemart_order_item_id, product_quantity, order_item_name,
 	 * @return boolean true if deleted successful, false if there was a problem
 	 */
 	function deleteInvoice($order_id ) {
-		$db = JFactory::getDBO();
 
-		$q = 'SELECT * FROM `#__virtuemart_invoices` WHERE `virtuemart_order_id`= "'.$order_id.'" ';
-
-		$db->setQuery($q);
-		$data = $db->loadAssoc();
-		if(!$data or   empty($data['invoice_number']) ){
-			return true;
-		}
-
-		// rename invoice pdf file
-		$invoice_prefix='vminvoice_';
-		$path = shopFunctions::getInvoicePath(VmConfig::get('forSale_path',0));
-		$invoice_name_src = $path.DS.$invoice_prefix.$data['invoice_number'].'.pdf';
-
-		if(!file_exists($invoice_name_src)){
-			// may be it was already deleted when changing order items
-			$data['invoice_number'] = $data['invoice_number'].' not found.';
-		} else {
-			$date = date("Ymd");
-			$data['invoice_number'] = $data['invoice_number'].'_'.$date;
-			$invoice_name_dst = $path.DS.$data['invoice_number'].'.pdf';
-
-			if(!class_exists('JFile')) require(VMPATH_LIBS.DS.'joomla'.DS.'filesystem'.DS.'file.php');
-			if (!JFile::move($invoice_name_src, $invoice_name_dst)) {
-				vmError ('Could not rename Invoice '.$invoice_name_src.'to '. $invoice_name_dst );
-			}
-		}
-
-		$table = $this->getTable('invoices');
-		$table->bindChecknStore($data);
-
-		return true;
+		return $this->renameInvoice($order_id);
 	}
 
 }

components/com_virtuemart/language/en-GB/en-GB.com_virtuemart_orders.ini
@@ -8,6 +8,7 @@ COM_VIRTUEMART_ACC_ORDER_INFO="Order Information"
 COM_VIRTUEMART_DELIVERY_DATE="Delivery Date"
 COM_VIRTUEMART_DELDATE_INV="Same as invoice date"
 COM_VIRTUEMART_INVOICE="Invoice"
+COM_VIRTUEMART_INV_FPREFIX="vminvoice_"
 COM_VIRTUEMART_INVOICE_CREATOR="Invoice by VirtueMart 2"
 COM_VIRTUEMART_INVOICE_DATE="Invoice date"
 COM_VIRTUEMART_INVOICE_SUBJ="Invoice Nr. %2$s (Order %3$s), %1$s"

administrator/components/com_virtuemart/helpers/shopfunctions.php
@@ -1044,41 +1044,48 @@ class ShopFunctions {
 		return $html;
 	}
 
-	static $tested = False;
+	static $safePath = '';
 	static function checkSafePath($safePath=0){
 
 		static $warned = false;
-		if($safePath==0) {
-			$safePath = VmConfig::get('forSale_path',0);
-			if(self::$tested) return $safePath;
+		if(!empty(self::$safePath)) {
+			return self::$safePath;
+		} else if (!self::$safePath){
+			return false;
 		}
 
-		$warn = FALSE;
+		if($safePath==0) {
+			self::$safePath = VmConfig::get('forSale_path',0);
+		} else {
+			self::$safePath = $safePath;
+		}
+
+		$warn = false;
 		$uri = JFactory::getURI();
-		$configlink = $uri->root() . 'administrator/index.php?option=com_virtuemart&view=config';
+
 		VmConfig::loadJLang('com_virtuemart');
-		if(empty($safePath)){
+		if(empty(self::$safePath)){
 			$warn = 'COM_VIRTUEMART_WARN_NO_SAFE_PATH_SET';
 		} else {
 			if(!class_exists('JFolder')) require_once(VMPATH_LIBS.DS.'joomla'.DS.'filesystem'.DS.'folder.php');
-			$exists = JFolder::exists($safePath);
+			$exists = JFolder::exists(self::$safePath);
 			if(!$exists){
 				$warn = 'COM_VIRTUEMART_WARN_SAFE_PATH_WRONG';
 			} else{
-				if(!is_writable( $safePath )){
+				if(!is_writable( self::$safePath )){
 					VmConfig::loadJLang('com_virtuemart_config');
 					if(!$warned){
-						vmdebug('checkSafePath $safePath not writeable '.$safePath);
-						VmError(vmText::sprintf('COM_VIRTUEMART_WARN_SAFE_PATH_INV_NOT_WRITEABLE',vmText::_('COM_VIRTUEMART_ADMIN_CFG_MEDIA_FORSALE_PATH'),$safePath)
+						vmdebug('checkSafePath $safePath not writeable '.self::$safePath);
+						VmError(vmText::sprintf('COM_VIRTUEMART_WARN_SAFE_PATH_INV_NOT_WRITEABLE',vmText::_('COM_VIRTUEMART_ADMIN_CFG_MEDIA_FORSALE_PATH'),self::$safePath)
 						,vmText::sprintf('COM_VIRTUEMART_WARN_SAFE_PATH_INV_NOT_WRITEABLE','',''));
 						$warned = true;
 					}
 				} else {
-					if(!is_writable(self::getInvoicePath($safePath) )){
+					if(!is_writable(self::getInvoicePath(self::$safePath) )){
 						VmConfig::loadJLang('com_virtuemart_config');
 						if(!$warned){
-							vmdebug('checkSafePath $safePath/invoice not writeable '.addslashes($safePath));
-							VmError(vmText::sprintf('COM_VIRTUEMART_WARN_SAFE_PATH_INV_NOT_WRITEABLE',vmText::_('COM_VIRTUEMART_ADMIN_CFG_MEDIA_FORSALE_PATH'),$safePath)
+							vmdebug('checkSafePath $safePath/invoice not writeable '.addslashes(self::$safePath));
+							VmError(vmText::sprintf('COM_VIRTUEMART_WARN_SAFE_PATH_INV_NOT_WRITEABLE',vmText::_('COM_VIRTUEMART_ADMIN_CFG_MEDIA_FORSALE_PATH'),self::$safePath)
 							,vmText::sprintf('COM_VIRTUEMART_WARN_SAFE_PATH_INV_NOT_WRITEABLE','',''));
 							$warned = true;
 						}
@@ -1088,14 +1095,15 @@ class ShopFunctions {
 		}
 
 		if($warn and !$warned){
+			self::$safePath = false;
 			$suggestedPath=shopFunctions::getSuggestedSafePath();
 			VmConfig::loadJLang('com_virtuemart_config');
+			$configlink = $uri->root() . 'administrator/index.php?option=com_virtuemart&view=config';
 			VmError(vmText::sprintf($warn,vmText::_('COM_VIRTUEMART_ADMIN_CFG_MEDIA_FORSALE_PATH'),$suggestedPath,$configlink));
 			$warned = true;
-			return FALSE;
 		}
 
-		return $safePath;
+		return self::$safePath;
 	}
 	/*
 	 * get The invoice Folder Name

administrator/components/com_virtuemart/models/user.php
@@ -368,7 +368,7 @@ class VirtueMartModelUser extends VmModel {
 			$usersConfig = JComponentHelper::getParams( 'com_users' );
 
 			$cUser = JFactory::getUser();
-			if(!($cUser->authorise('core.admin','com_virtuemart') or $cUser->authorise('core.manage','com_virtuemart') or $cUser->authorise('vm.user', 'com_virtuemart')) and $usersConfig->get('allowUserRegistration') == '0') {
+			if($usersConfig->get('allowUserRegistration') == '0' and !($cUser->authorise('core.admin','com_virtuemart') or $cUser->authorise('core.manage','com_virtuemart') or $cUser->authorise('vm.user', 'com_virtuemart')) ) {
 				VmConfig::loadJLang('com_virtuemart');
 				vmError( vmText::_('COM_VIRTUEMART_ACCESS_FORBIDDEN'));
 				return;

administrator/components/com_virtuemart/plugins/vmpsplugin.php
@@ -1052,7 +1052,7 @@ abstract class vmPSPlugin extends vmPlugin {
 			$cart_prices[$this->_psType . 'Tax'] = 0;
 			$cart_prices[$this->_psType . '_calc_id'] = 0;
 		}
-
+		//if($_psType='Shipment')vmTrace('setCartPrices '.$cart_prices['salesPrice' . $_psType]);
 		return $cart_prices['salesPrice' . $_psType];
 
 	}

components/com_virtuemart/controllers/cart.php
@@ -162,17 +162,19 @@ class VirtueMartControllerCart extends JControllerLegacy {
 			}
 		}
 
+		$force = VmConfig::get('oncheckout_opc',true);
+		$cart->setShipmentMethod($force, !$html);
+		$cart->setPaymentMethod($force, !$html);
+
 		$cart->prepareCartData();
 
 		$coupon_code = trim(vRequest::getString('coupon_code', ''));
 		if(!empty($coupon_code)){
-
 			$msg = $cart->setCouponCode($coupon_code);
 			if($msg) vmInfo($msg);
 		}
-		$force = VmConfig::get('oncheckout_opc',true);
-		$cart->setShipmentMethod($force, !$html);
-		$cart->setPaymentMethod($force, !$html);
+
+
 		if ($html) {
 			$this->display();
 		} else {

components/com_virtuemart/controllers/invoice.php
@@ -221,7 +221,8 @@ class VirtueMartControllerInvoice extends JControllerLegacy
 			return 0;
 		}
 		
-		$path .= preg_replace('/[^A-Za-z0-9_\-\.]/', '_', 'vm'.$layout.'_'.$invoiceNumber.'.pdf');
+		//$path .= preg_replace('/[^A-Za-z0-9_\-\.]/', '_', 'vm'.$layout.'_'.$invoiceNumber.'.pdf');
+		$path .= shopFunctionsF::getInvoiceName($invoiceNumber).'.pdf';
 
 		if(file_exists($path) and !$force){
 			return $path;

components/com_virtuemart/helpers/cart.php
@@ -816,6 +816,7 @@ class VirtueMartCart {
 			$dataValid = true;
 			foreach ($_retValues as $_retVal) {
 				if ($_retVal === true ) {
+					$this->setCartIntoSession();
 					// Plugin completed successfull; nothing else to do
 					break;
 				} else if ($_retVal === false ) {
@@ -848,6 +849,7 @@ class VirtueMartCart {
 			$dataValid = true;
 			foreach ($_retValues as $_retVal) {
 				if ($_retVal === true ) {
+					$this->setCartIntoSession();
 					// Plugin completed succesfull; nothing else to do
 					break;
 				} else if ($_retVal === false ) {
@@ -1374,7 +1376,7 @@ class VirtueMartCart {
 				}
 			}
 		} else {
-			vmdebug('getST STsameAsBT is set, use BT');
+			//vmdebug('getST STsameAsBT is set, use BT');
 			$addr = $this->BT;
 		}
 

commit 45a7223e570eafa7deb65041cfaad46c7f24029a

Fix the invoice file naming system (broken by r8821)
-) The function shopFunctionsF::getInvoiceName needs to filter special characters like forward slashes or colons in the invoice number
-) The function shopFunctionsF::getInvoiceName also needs the layout as argument, so it works with delivery notes, too
-) Added file prefix for delivery notes
-) The Order model tried calling getInvoiceName from the wrong class (=> fatal error)
-) When modifying an order in the BE, the invoice is renamed by simply appending _deprecated and the current date (previously, it was renamed to the invoice number without a prefix)

administrator/components/com_virtuemart/models/orders.php
@@ -1989,8 +1989,10 @@ $q = 'SELECT virtuemart_order_item_id, product_quantity, order_item_name,
 			//$data['invoice_number'] = $data['invoice_number'].' not found.';
 		} else {
 			$date = date("Ymd");
+			// We change the invoice number in the invoice table only. The order's invoice number is not modified!
 			$data['invoice_number'] = $table->invoice_number.'_'.$date;
-			$invoice_name_dst = $path.DS.$table->invoice_number.'.pdf';
+			// We need to sanitize the file name as the invoice number might contain strange characters like 2015/01.
+			$invoice_name_dst = $path.DS.$name.'_deprecated'.$date.'.pdf';
 
 			if(!class_exists('JFile')) require(VMPATH_LIBS.DS.'joomla'.DS.'filesystem'.DS.'file.php');
 			if (!JFile::move($invoice_name_src, $invoice_name_dst)) {

components/com_virtuemart/language/en-GB/en-GB.com_virtuemart_orders.ini
@@ -8,7 +8,8 @@ COM_VIRTUEMART_ACC_ORDER_INFO="Order Information"
 COM_VIRTUEMART_DELIVERY_DATE="Delivery Date"
 COM_VIRTUEMART_DELDATE_INV="Same as invoice date"
 COM_VIRTUEMART_INVOICE="Invoice"
-COM_VIRTUEMART_INV_FPREFIX="vminvoice_"
+COM_VIRTUEMART_FILEPREFIX_INVOICE="vminvoice_"
+COM_VIRTUEMART_FILEPREFIX_DELIVERYNOTE="vmdeliverynote_"
 COM_VIRTUEMART_INVOICE_CREATOR="Invoice by VirtueMart 2"
 COM_VIRTUEMART_INVOICE_DATE="Invoice date"
 COM_VIRTUEMART_INVOICE_SUBJ="Invoice Nr. %2$s (Order %3$s), %1$s"