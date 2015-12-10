c492c2db0f66ddf54906a0ae2103a1a1635feb94
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2015-12-10 09:21:25 +0000

    Realex: handling of 501: order already processed: do not change previous order status
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@9081 87cc427b-4a1e-0410-b729-b2260ec0f063

--- plugins/vmpayment/realex_hpp_api/realex_hpp_api.php
+++ plugins/vmpayment/realex_hpp_api/realex_hpp_api.php
@@ -1751,6 +1751,7 @@ class plgVmPaymentRealex_hpp_api extends vmPSPlugin {
 			}
 		} else {
 			$msgToShopper='';
+			$status = $this->_currentMethod->status_canceled;
 			if ($realexInterface->isResponseDeclined($xml_response3DSVerifysig)) {
 				$order_history['comments'] = vmText::sprintf('VMPAYMENT_REALEX_HPP_API_PAYMENT_DECLINED', $realexInterface->order['details']['BT']->order_number);
 				$msgToShopper=$xml_response3DSVerifysig->message;
@@ -1760,6 +1761,9 @@ class plgVmPaymentRealex_hpp_api extends vmPSPlugin {
 			} elseif ($realexInterface->isResponseAlreadyProcessed($xml_response3DSVerifysig)) {
 				$order_history['comments'] = $xml_response3DSVerifysig->message;
 				$msgToShopper=$xml_response3DSVerifysig->message;
+				// log this response, but do not change the order status
+				$status = $realexInterface->order['details']['BT']->order_status;
+
 				/* } elseif ($xml_response and $realexInterface->isResponseInvalidPaymentDetails($xml_response)) {
 
 					$order_history['comments'] =$xml_response->message;
@@ -1775,13 +1779,11 @@ class plgVmPaymentRealex_hpp_api extends vmPSPlugin {
 				$order_history['comments'] = vmText::sprintf('VMPAYMENT_REALEX_HPP_API_PAYMENT_STATUS_CANCELLED', $realexInterface->order['details']['BT']->order_number);
 			}
 			$redirectToCart = true;
-			$status = $this->_currentMethod->status_canceled;
 		}
 
 		$order_history['customer_notified'] = true;
 		$order_history['order_status'] = $status;
 		//	$this->updateOrderStatus($realexInterface->order, $redirectToCart);
-
 		$modelOrder = VmModel::getModel('orders');
 		$modelOrder->updateStatusForOneOrder($realexInterface->order['details']['BT']->virtuemart_order_id, $order_history, false);
 
