69784ce8d8f085c38c378080d663d70ea2588fab
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2015-12-15 11:53:34 +0000

    Canonical: absolute URLs for products too
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@9087 87cc427b-4a1e-0410-b729-b2260ec0f063

--- components/com_virtuemart/views/askquestion/view.html.php
+++ components/com_virtuemart/views/askquestion/view.html.php
@@ -92,7 +92,7 @@ class VirtueMartViewAskquestion extends VmView {
 		// Set Canonic link
 		$format = vRequest::getCmd('format', 'html');
 		if ($format == 'html') {
-			$document->addHeadLink ($product->canonical, 'canonical', 'rel', '');
+			$document->addHeadLink (JRoute::_($product->canonical, FALSE), 'canonical', 'rel', '');
 		}
 
 		// Set the titles
