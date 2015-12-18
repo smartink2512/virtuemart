df6f54ef0929c0bfb3bb138b484a3ce217754474
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2015-12-18 12:35:07 +0000

    Customer_number: adding security
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/trunk/virtuemart@9096 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart/models/user.php
+++ administrator/components/com_virtuemart/models/user.php
@@ -518,15 +518,17 @@ class VirtueMartModelUser extends VmModel {
 			}
 		}
 
+		if(!vmAccess::manager('user.edit')){
+			unset($data['customer_number']);
+		}
 		if(empty($alreadyStoredUserData->customer_number)){
-			//if(!class_exists('vmUserPlugin')) require(VMPATH_SITE.DS.'helpers'.DS.'vmuserplugin.php');
-			///if(!$returnValues){
-			$data['customer_number'] = strtoupper(substr($data['username'],0,2)).substr(md5($data['username']),0,9);
-			//We set this data so that vmshopper plugin know if they should set the customer number
-			$data['customer_number_bycore'] = 1;
-			//}
+			if(empty($data['customer_number'])){
+				$data['customer_number'] = strtoupper(substr($data['username'],0,2)).substr(md5($data['username']),0,9);
+				//We set this data so that vmshopper plugin know if they should set the customer number
+				$data['customer_number_bycore'] = 1;
+			}
 		} else {
-			if(!vmAccess::manager()){
+			if(!vmAccess::manager('user.edit')){
 				$data['customer_number'] = $alreadyStoredUserData->customer_number;
 			}
 		}
