b84e093d50a88cba4bc1e31ba6e5e149ffb74b03
Milbo <Milbo@87cc427b-4a1e-0410-b729-b2260ec0f063>
2016-06-22 21:51:54 +0000

    adjusted js to work with class 4remove
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/branches/com_virtuemart.3.0.12.4@9240 87cc427b-4a1e-0410-b729-b2260ec0f063

--- administrator/components/com_virtuemart/assets/js/vm2admin.js
+++ administrator/components/com_virtuemart/assets/js/vm2admin.js
@@ -263,18 +263,20 @@
                 );
                 if ($.inArray(id, inputArray) == -1) {
                     that = jQuery(this);
-                    jQuery(this).clone().appendTo(container).unbind("click").append('<div class="vmicon vmicon-16-remove" title="remove"></div><div class="edit-24-grey" title="' + vm2string.editImage + '"><div>');
+                    jQuery(this).clone().appendTo(container).unbind("click").append('<div class="vmicon vmicon-16-remove 4remove" title="remove"></div><div class="edit-24-grey" title="' + vm2string.editImage + '"><div>');
                     that.hide().fadeIn();
                 }
 
             });
 
-            jQuery("#admin-ui-tabs").delegate("div.vmicon-16-remove", "click", function () {
+            jQuery("#admin-ui-tabs").delegate("div.4remove", "click", function () {
+                console.log('remove',this);
                 jQuery(this).closest(".vm_thumb_image").fadeOut("500", function () {
                     jQuery(this).remove();
                 });
             });
-            jQuery("#admin-ui-tabs").delegate("span.vmicon-16-remove", "click", function () {
+            jQuery("#admin-ui-tabs").delegate("span.4remove", "click", function () {
+                console.log('remove 2',this);
                 jQuery(this).closest(".removable").fadeOut("500", function () {
                     jQuery(this).remove()
                 });
