f08fc25630ab0b316677d02b1ea571f59c71fe73
Milbo <Milbo@87cc427b-4a1e-0410-b729-b2260ec0f063>
2017-01-29 21:00:25 +0000

    fix for related products, destroyed the ajax of multivariants update by Stefan Schumacher for the 3 price lists (cart, invoice, orders) removed old tags against inline style (yeh, class comes later)
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/branches/com_virtuemart.3.0.12.4@9439 87cc427b-4a1e-0410-b729-b2260ec0f063

--- components/com_virtuemart/sublayouts/related.php
+++ components/com_virtuemart/sublayouts/related.php
@@ -6,7 +6,9 @@ $thumb = $viewData['thumb'];
 
 
 //juri::root() For whatever reason, we used this here, maybe it was for the mails
-?><div class="vm-product-media-container"><?php
+?>
+<div class="product-container">
+<div class="vm-product-media-container"><?php
 echo JHtml::link (JRoute::_ ('index.php?option=com_virtuemart&view=productdetails&virtuemart_product_id=' . $related->virtuemart_product_id . '&virtuemart_category_id=' . $related->virtuemart_category_id), $thumb   . $related->product_name, array('title' => $related->product_name,'target'=>'_blank'));
 ?></div><?php
 if($customfield->wPrice){
@@ -25,3 +27,4 @@ if($customfield->waddtocart){
 if($customfield->wDescr){
 	echo '<p class="product_s_desc">'.$related->product_s_desc.'</p>';
 }
+	?></div>
