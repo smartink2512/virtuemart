7f0c1ea997c0326a6218c85af4e00654a35fec4f
Milbo <Milbo@87cc427b-4a1e-0410-b729-b2260ec0f063>
2017-05-14 11:05:13 +0000

    small enhancements for extra images in productdetails no unecessary initiation of unused scripts
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/branches/com_virtuemart.3.0.12.4@9542 87cc427b-4a1e-0410-b729-b2260ec0f063

--- components/com_virtuemart/views/productdetails/tmpl/default_images_additional.php
+++ components/com_virtuemart/views/productdetails/tmpl/default_images_additional.php
@@ -28,10 +28,10 @@ defined('_JEXEC') or die('Restricted access');
 		<div class="floatleft">
 			<?php
 			if(VmConfig::get('add_img_main', 1)) {
-				echo $image->displayMediaThumb('class="product-image" style="cursor: pointer"',false,$image->file_description);
+				echo $image->displayMediaThumb('class="product-image" style="cursor: pointer" data-descr="'.$image->file_description.'"',false,'',true);
 				echo '<a href="'. $image->file_url .'"  class="product-image image-'. $i .'" style="display:none;" title="'. $image->file_meta .'" rel="vm-additional-images"></a>';
 			} else {
-				echo $image->displayMediaThumb("",true,"rel='vm-additional-images'",true,$image->file_description);
+				echo $image->displayMediaThumb('',true,"rel='vm-additional-images'",true,$image->file_description);
 			}
 			?>
 		</div>
