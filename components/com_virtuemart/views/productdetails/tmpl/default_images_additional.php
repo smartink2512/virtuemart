a867bae21c58dda46afe8c39910d619d7bc4926b
Milbo <Milbo@87cc427b-4a1e-0410-b729-b2260ec0f063>
2017-05-15 07:24:19 +0000

    description for additional images in the code (for overriders easier to see/understand)
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart/branches/com_virtuemart.3.0.12.4@9543 87cc427b-4a1e-0410-b729-b2260ec0f063

--- components/com_virtuemart/views/productdetails/tmpl/default_images_additional.php
+++ components/com_virtuemart/views/productdetails/tmpl/default_images_additional.php
@@ -24,14 +24,15 @@ defined('_JEXEC') or die('Restricted access');
 	$start_image = VmConfig::get('add_img_main', 1) ? 0 : 1;
 	for ($i = $start_image; $i < count($this->product->images); $i++) {
 		$image = $this->product->images[$i];
+		$descr = '';// $image->file_description;
 		?>
 		<div class="floatleft">
 			<?php
 			if(VmConfig::get('add_img_main', 1)) {
-				echo $image->displayMediaThumb('class="product-image" style="cursor: pointer" data-descr="'.$image->file_description.'"',false,'',true);
+				echo $image->displayMediaThumb('class="product-image" style="cursor: pointer" data-descr="'.$image->file_description.'"',false,'',true,$descr);
 				echo '<a href="'. $image->file_url .'"  class="product-image image-'. $i .'" style="display:none;" title="'. $image->file_meta .'" rel="vm-additional-images"></a>';
 			} else {
-				echo $image->displayMediaThumb('',true,"rel='vm-additional-images'",true,$image->file_description);
+				echo $image->displayMediaThumb('',true,"rel='vm-additional-images'",true,$descr);
 			}
 			?>
 		</div>
